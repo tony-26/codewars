1e5f189db6f681451d4ec3552afd504b2662be26
Tony Wang <wangh9947@gmail.com>
2022-10-07 00:47:54 +0100

    simple multi... complete

---
+++ kyu8/fillingArray.js
@@ -0,0 +1,10 @@
+const array = (arr) => {
+  let newArr = [];
+  for (i = 0; i < arr; i++) {
+    newArr.push(i);
+  }
+  return newArr;
+};
+console.log(array(9)); //4
+
+// Filling an array
---
+++ kyu8/simpleMultiplication.js
@@ -0,0 +1,8 @@
+const simpleMultiplication = (number) => {
+  if (number % 2 === 0) {
+    number = number * 8;
+  } else {
+    number = number * 9;
+  }
+  return number;
+};
