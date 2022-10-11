87728a792caef649f59133d931ce7af7b75480c0
Tony Wang <wangh9947@gmail.com>
2022-10-11 22:44:35 +0100

    'create phone number' complete

---
+++ kyu8/createPhoneNumber.js
@@ -0,0 +1,20 @@
+const createPhoneNumber = (numbers) => {
+  let phoneNum = "";
+  phoneNum =
+    "(" +
+    numbers[0] +
+    numbers[1] +
+    numbers[2] +
+    ") " +
+    numbers[3] +
+    numbers[4] +
+    numbers[5] +
+    "-" +
+    numbers[6] +
+    numbers[7] +
+    numbers[8] +
+    numbers[9];
+  return phoneNum;
+};
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
+//Create Phone Number
