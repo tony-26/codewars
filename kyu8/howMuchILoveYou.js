a396b3fee84d5203d8f89b82692ee50a87c2de24
Tony Wang <wangh9947@gmail.com>
2022-10-11 14:55:56 +0100

    'first attempt of how much i love you'

---
+++ kyu8/howMuchILoveYou.js
@@ -0,0 +1,18 @@
+const howMuchILoveYou = (nbPetals) => {
+  const anum = {
+    1: "I love you",
+    2: "a little",
+    3: "a lot",
+    4: "passionately",
+    5: "madly",
+    6: "not at all",
+  };
+  if (nbPetals <= 6) {
+    return anum[nbPetals];
+  } else {
+    return anum[nbPetals % 6];
+  }
+};
+console.log(howMuchILoveYou(162));
+
+//I love you, a little....
