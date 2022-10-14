e8d169b9b5bed3df944b79476cf67b5709ad86bb
Tony Wang <wangh9947@gmail.com>
2022-10-14 01:12:37 +0100

    'likes' complete

--- kyu8/createPhoneNumber.js
+++ kyu8/createPhoneNumber.js
@@ -1,20 +1,21 @@
 const createPhoneNumber = (numbers) => {
-  let phoneNum = "";
-  phoneNum =
-    "(" +
-    numbers[0] +
-    numbers[1] +
-    numbers[2] +
-    ") " +
-    numbers[3] +
-    numbers[4] +
-    numbers[5] +
-    "-" +
-    numbers[6] +
-    numbers[7] +
-    numbers[8] +
-    numbers[9];
-  return phoneNum;
+  //   let phoneNum = "";
+  //   phoneNum =
+  //     "(" +
+  //     numbers[0] +
+  //     numbers[1] +
+  //     numbers[2] +
+  //     ") " +
+  //     numbers[3] +
+  //     numbers[4] +
+  //     numbers[5] +
+  //     "-" +
+  //     numbers[6] +
+  //     numbers[7] +
+  //     numbers[8] +
+  //     numbers[9];
+  //   return phoneNum;
+  return numbers.join();
 };
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
 //Create Phone Number
--- kyu8/likes.js
+++ kyu8/likes.js
@@ -1,19 +1,21 @@
 const likes = (names) => {
   let output = "";
   let str = "";
-  str = names.toString().split(",");
+
   let count = 0;
   // console.log(str);
 
-  if (names === []) {
+  if (names.length === 0) {
     output = output + "no one likes this";
-  } else if (str.length === 2) {
-    output = str.join(" and") + "likes this";
-  } else if (str.length === 3) {
-    output = str[0] + ", " + str[1] + " and " + str[2] + " like this";
-  } else if (str.length > 3) {
-    count = str.length - 2;
-    output = str[0] + ", " + str[1] + " and " + count + " others like this";
+  } else if (names.length === 1) {
+    output = names[0] + " likes this";
+  } else if (names.length === 2) {
+    output = names.join(" and ") + " like this";
+  } else if (names.length === 3) {
+    output = names[0] + ", " + names[1] + " and " + names[2] + " like this";
+  } else if (names.length > 3) {
+    count = names.length - 2;
+    output = names[0] + ", " + names[1] + " and " + count + " others like this";
   }
 
   return output;
