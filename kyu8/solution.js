9c3152b88d1f01b876d02aa9c30d37d7d5c2ee7a
Tony Wang <wangh9947@gmail.com>
2022-10-16 21:10:52 +0100

    solution 1st att

---
+++ kyu8/solution.js
@@ -0,0 +1,22 @@
+const solution = (number) => {
+  let mul3 = [];
+  let mul5 = [];
+  let sum = 0;
+  if (number < 0) {
+    return 0;
+  } else {
+    for (i = 1; i < number / 3; i++) {
+      //   mul3.push(3 * i);
+      sum = sum + 3 * i;
+    }
+    for (i = 1; i < number / 5; i++) {
+      //   mul5.push(5 * i);
+      sum = sum + 5 * i;
+    }
+  }
+  return sum;
+};
+console.log(solution(10)); // 23
+console.log(solution(2801)); // 1831668
+
+// multiples of 3 or 5
