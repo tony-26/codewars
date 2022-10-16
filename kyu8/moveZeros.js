5f6f1cf5dd03fa2b1ee89a452ebd3ecb4a24ed69
Tony Wang <wangh9947@gmail.com>
2022-10-16 20:55:55 +0100

    'number to string' complete

--- kyu8/moveZeros.js
+++ kyu8/moveZeros.js
@@ -13,6 +13,8 @@ const moveZeros = (arr) => {
   return arr;
 };
 console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
-console.log([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0]);
+console.log(
+  moveZeros([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0])
+);
 
 //moving zeros to the end
---
+++ kyu8/numberToString.js
@@ -0,0 +1,3 @@
+const numberToString = (num) => {
+  return num.toString();
+};
