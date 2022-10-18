e4a6804504b3b680be5a7f0d8bd9643ba47d2257
Tony Wang <wangh9947@gmail.com>
2022-10-18 17:08:01 +0100

    'find smallest in arr' complete

---
+++ kyu8/SmallestIntegerFinder.js
@@ -0,0 +1,5 @@
+const findSmallestInt = (args) => {
+  min = Math.min(...args);
+  return min;
+};
+console.log(findSmallestInt([1, 56, 232, 12, 8])); // 1
--- kyu8/test.js
+++ kyu8/test.js
@@ -1,4 +1,7 @@
 //testing
-const str = "hello";
-const str2 = str.charAt(0).toUpperCase() + str.slice(1);
-console.log(str2);
+// const str = "hello";
+// const str2 = str.charAt(0).toUpperCase() + str.slice(1);
+// console.log(str2);
+const arr = [14, 58, 20, 77, 66, 2, 42, 67, 42, 4];
+const min = Math.min(...arr);
+console.log(min);
