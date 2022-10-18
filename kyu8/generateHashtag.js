bf431ac3fb3247e0432b26655208d08b9fdadb79
Tony Wang <wangh9947@gmail.com>
2022-10-18 19:03:37 +0100

    'generate hash tag' cmoplete

--- kyu8/generateHashtag.js
+++ kyu8/generateHashtag.js
@@ -1,17 +1,13 @@
 const generateHashtag = (str) => {
   let str1 = "#";
-  let str2 = "";
   if (str.replace(/\s/g, "").length === 0) {
     return false;
-  } else {
-    arr1 = str.split(" ");
-    for (i = 0; i < arr1.length; i++) {
-      str2 = arr1[i].charAt(0).toUpperCase() + arr1[i].slice(1);
-      str1 = str1 + str2;
-    }
-
-    // console.log(arr1);
   }
+
+  str.split(" ").forEach((e) => {
+    str1 = str1 + e.charAt(0).toUpperCase() + e.slice(1);
+  });
+
   if (str1.length > 140) {
     return false;
   } else {
@@ -19,8 +15,8 @@ const generateHashtag = (str) => {
   }
 };
 
-// console.log(generateHashtag("Hello how are you")); // "#HelloHowAreYou"
-// console.log(generateHashtag("")); //false
+console.log(generateHashtag("Hello how are you")); // "#HelloHowAreYou"
+console.log(generateHashtag("")); //false
 console.log(generateHashtag(" ".repeat(200))); // false
 
 //The Hashtag Generator
