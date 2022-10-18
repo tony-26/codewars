7a68def28bc3ab8188673bf42874ffca7dc2042a
Tony Wang <wangh9947@gmail.com>
2022-10-18 18:43:12 +0100

    'get sum' complete

--- kyu8/getSum.js
+++ kyu8/getSum.js
@@ -3,17 +3,17 @@ const getSum = (a, b) => {
   if (a === b) {
     return a;
   } else if (a < b) {
-    for (i = a; i < b - a + 1; i++) {
+    for (i = a; i <= b; i++) {
       sum = sum + i;
     }
   } else if (a > b) {
-    for (i = b; i < a - b; i++) {
+    for (i = b; i <= a; i++) {
       sum = sum + i;
     }
   }
   return sum;
 };
-console.log(getSum(0, 1)); //1
-console.log(getSum(0, -1)); //-1
-console.log(getSum(-347, 518)); // 74043
+// console.log(getSum(0, 1)); //1
+// console.log(getSum(0, -1)); //-1
+console.log(getSum(-347, 518)); // 74043          a = -347, d = 1, n = 518 - (-347) + 1, s = n/2 * (2a + (n - 1) * d)
 //Beginner Series
