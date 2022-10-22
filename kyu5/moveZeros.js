976af936bf403657166010e3140fcc6288601480
Tony Wang <wangh9947@gmail.com>
2022-10-22 09:45:57 +0100

    'move zeros' refactored

--- kyu5/moveZeros.js
+++ kyu5/moveZeros.js
@@ -1,22 +1,23 @@
 const moveZeros = (arr) => {
   let newArr = [];
-  let len = 0;
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] !== 0) {
       newArr.push(arr[i]);
     }
   }
-  len = arr.length - newArr.length;
+  const len = arr.length - newArr.length;
   for (let i = 0; i < len; i++) {
     newArr.push(0);
-    // console.log(newArr.length);
   }
   return newArr;
 };
 // console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])); // [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
-// console.log(
-//   moveZeros([9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0]) // []
-// );
 
-console.log(moveZeros(["a", "b", null, "c", "d", 1, 1, 3, 1, 9, {}, 9, 0, 0]));
-//moving zeros to the end
+// console.log(moveZeros(["a", "b", null, "c", "d", 1, 1, 3, 1, 9, {}, 9, 0, 0]));
+
+const moveZerosV2 = (array) =>
+  array.filter((e) => e !== 0).concat(array.filter((e) => e === 0));
+console.log(
+  moveZerosV2([0, 0, "a", "b", null, "c", "d", 1, 1, 3, 1, 9, {}, 9, 0, 0])
+);
