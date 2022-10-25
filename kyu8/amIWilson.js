65f39900b033cab4e32c2cee7fea4d3b3eb575fd
Tony Wang <wangh9947@gmail.com>
2022-10-25 02:48:33 +0100

    'prime number' 1st att

---
+++ kyu8/amIWilson.js
@@ -0,0 +1,10 @@
+const amIWilson = (p) => {
+  for (let i = 2; i <= p; i++) {
+    if (p % i === 0) {
+      return false;
+    }
+    return true;
+  }
+};
+console.log(amIWilson(5)); // true
+console.log(amIWilson(9)); // false
