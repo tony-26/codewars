1e8ec78185246f43ca2e4d627b1042756e09c7f9
Tony Wang <wangh9947@gmail.com>
2022-10-25 02:31:38 +0100

    'solution' final version

--- kyu6/solution.js
+++ kyu6/solution.js
@@ -1,25 +1,19 @@
 const solution = (number) => {
-  let mul3 = [];
-  let mul5 = [];
   let sum = 0;
   if (number < 0) {
     return 0;
   } else {
     for (i = 1; i < number / 3; i++) {
-      // console.log(i);
-      //   mul3.push(3 * i);
       sum = sum + 3 * i;
     }
     for (i = 1; i < number / 5; i++) {
       if (i % 3 !== 0) {
-        //   mul5.push(5 * i);
         sum = sum + 5 * i;
       }
     }
   }
   return sum;
 };
-// console.log(solution(10)); // 23
 console.log(solution(20)); // 78
 
 // multiples of 3 or 5
