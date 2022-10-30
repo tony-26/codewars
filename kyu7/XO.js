37aca2ed24e8f1a0e977eecaf04d61a16e434ee7
Tony Wang <wangh9947@gmail.com>
2022-10-30 19:58:32 +0000

    'XOs' final version

--- kyu7/XO.js
+++ kyu7/XO.js
@@ -10,15 +10,13 @@ const XO = (str) => {
   }
   if (countX === countO) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 
 console.log(XO("xo"), true);
 console.log(XO("xxOo"), true);
 console.log(XO("xxxm"), false);
 console.log(XO("Oo"), false);
-// console.log(XO("ooom"), false);
+console.log(XO("ooom"), false);
 //Exes and Ohs
-//passing all tests
