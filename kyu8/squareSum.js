31607e13a00b5fac011d22887e48f7277db5fbdf
Tony Wang <wangh9947@gmail.com>
2022-10-31 20:48:05 +0000

    'square sum' final version

--- kyu8/squareSum.js
+++ kyu8/squareSum.js
@@ -5,6 +5,6 @@ const squareSum = (numbers) => {
   }
   return sum;
 };
-console.log(squareSum([1, 2]), 5);
-console.log(squareSum([0, 3, 4, 5]), 50);
-console.log(squareSum([]), 0);
+console.log(squareSum([1, 2]) === 5);
+console.log(squareSum([0, 3, 4, 5]) === 50);
+console.log(squareSum([]) === 0);
