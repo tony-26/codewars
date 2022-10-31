bb15bb69e99cdbff4e182ca77a6f04fced7ddf61
Tony Wang <wangh9947@gmail.com>
2022-10-31 20:51:06 +0000

    'paperwork' passing all tests

---
+++ kyu8/paperwork.js
@@ -0,0 +1,12 @@
+const paperwork = (n, m) => {
+  if (n < 0 || m < 0) {
+    return 0;
+  }
+  return n * m;
+};
+
+console.log(paperwork(5, 5), 25);
+console.log(paperwork(5, -5), 0);
+console.log(paperwork(-5, -5), 0);
+console.log(paperwork(-5, 5), 0);
+console.log(paperwork(5, 0), 0);
