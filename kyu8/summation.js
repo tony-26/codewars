0578697e11b569211f5cf031ca89975648c81933
Tony Wang <wangh9947@gmail.com>
2022-11-02 23:55:03 +0000

    'summation' complete

---
+++ kyu8/summation.js
@@ -0,0 +1,11 @@
+const summation = (num) => {
+  let sum = 0;
+  for (i = 1; i <= num; i++) {
+    sum = sum + i;
+  }
+  return sum;
+};
+
+console.log(summation(1), 1);
+console.log(summation(2), 3);
+console.log(summation(8), 36);
