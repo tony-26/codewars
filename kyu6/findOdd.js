ada6fe7a89cbe6d469bdac859d3b663ff18783c6
Tony Wang <wangh9947@gmail.com>
2022-11-05 09:32:24 +0000

    'findOdd' passed all tests

--- kyu6/findOdd.js
+++ kyu6/findOdd.js
@@ -1,54 +1,26 @@
-const findOdd = (A) => {
-  let newArr = [];
-  let count0 = 0;
-  let count1 = 0;
-  let count2 = 0;
-  let count3 = 0;
-  let count4 = 0;
-  let count5 = 0;
-  let count6 = 0;
-  let count7 = 0;
-  let count8 = 0;
-  let count9 = 0;
-  let count10 = 0;
-  for (i = 0; i < A.length; i++) {
-    if (A[i] === 0) {
-      count0 += 1;
-    } else if (A[i] === 1) {
-      count1 += 1;
-    } else if (A[i] === 2) {
-      count2 += 1;
-    } else if (A[i] === 3) {
-      count3 += 1;
-    } else if (A[i] === 4) {
-      count4 += 1;
-    } else if (A[i] === 5) {
-      count5 += 1;
-    } else if (A[i] === 6) {
-      count6 += 1;
-    } else if (A[i] === 7) {
-      count7 += 1;
-    } else if (A[i] === 8) {
-      count8 += 1;
-    } else if (A[i] === 9) {
-      count9 += 1;
-    }
+const findOdd = (numberArr) => {
+  numberArr.sort((a, b) => b - a);
+  console.log(numberArr);
+  if (numberArr.length === 0) {
+    return 0;
   }
-  newArr.push(
-    count0,
-    count1,
-    count2,
-    count3,
-    count4,
-    count5,
-    count6,
-    count7,
-    count8,
-    count9
-  );
-  for (i = 0; i < newArr.length; i++) {
-    if (newArr[i] % 2 != 0) {
-      return i;
+  let i = 0;
+  let currentNum = numberArr[0];
+  let currentCount = 1;
+  while (i < numberArr.length) {
+    if (numberArr[i] === numberArr[i + 1]) {
+      currentCount += 1;
+    } else {
+      if (currentCount % 2 !== 0) {
+        return currentNum;
+      }
+      currentCount = 1;
+      currentNum = numberArr[i + 1];
     }
+    i = i + 1;
   }
 };
+console.log(
+  findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]),
+  5
+);
