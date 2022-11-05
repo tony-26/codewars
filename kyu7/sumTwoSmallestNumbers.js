31997fd09ea61c6b7807e998febe374b2b17967d
Tony Wang <wangh9947@gmail.com>
2022-11-05 00:22:33 +0000

    'sumTwoSmallestNumbers' final v

--- kyu7/sumTwoSmallestNumbers.js
+++ kyu7/sumTwoSmallestNumbers.js
@@ -3,7 +3,6 @@ const sumTwoSmallestNumbers = (numbers) => {
   for (let i = 0; i < numbers.length; i++) {
     for (let j = 0; j < numbers.length - 1; j++) {
       if (numbers[j] > numbers[j + 1]) {
-        // Swap Elements
         temp = numbers[j];
         numbers[j] = numbers[j + 1];
         numbers[j + 1] = temp;
