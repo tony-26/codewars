e33fd54dbee755a7b3c6d9a56159a737fe89eb2e
Tony Wang <wangh9947@gmail.com>
2022-11-05 00:00:58 +0000

    'reverseWords' final version

--- kyu7/reverseWords.js
+++ kyu7/reverseWords.js
@@ -4,16 +4,14 @@ const reverseWords = (str) => {
   let newArr = [];
 
   newArr = str.split(" ");
-  // console.log(newArr);
   for (i = 0; i < newArr.length; i++) {
     n = newArr[i].split("").reverse().join("");
-    // console.log(n);
     finalStr = finalStr + n + " ";
   }
   finalStr = finalStr.slice(0, -1);
   return finalStr;
 };
-// console.log(reverseWords("apple"), "elppa");
+console.log(reverseWords("apple") === "elppa");
 console.log(
   reverseWords("The quick brown fox jumps over the lazy dog.") ===
     "ehT kciuq nworb xof spmuj revo eht yzal .god"
