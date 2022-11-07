d1150ea45f97b3c0ef5b96f29063adb1f97456d3
Tony Wang <wangh9947@gmail.com>
2022-11-07 10:14:09 +0000

    reduce way

--- kyu8/sum.js
+++ kyu8/sum.js
@@ -7,6 +7,8 @@ const sum = (numbers) => {
     });
   }
 };
+console.log(sum([]), 0);
+console.log(sum([1, 5.2, 4, 0, -1]), 9.2);
 // const sum = (numbers) => {
 //   let SUM = 0;
 //   for (i = 0; i < numbers.length; i++) {
