a5a6cf90700a0eac9b4aa4e9cb4f99b78f0850ed
Tony Wang <wangh9947@gmail.com>
2022-11-07 10:12:04 +0000

    'sum' trying reduce

--- kyu8/sum.js
+++ kyu8/sum.js
@@ -1,16 +1,16 @@
-// const sum = (numbers) => {
-//   if (numbers.length === 0) {
-//     return 0;
-//   } else {
-//     numbers.reduce((prevReturnedValue, currentNum) => {
-//       return currentNum + prevReturnedValue;
-//     });
-//   }
-// };
 const sum = (numbers) => {
-  let SUM = 0;
-  for (i = 0; i < numbers.length; i++) {
-    SUM = SUM + numbers[i];
+  if (numbers.length === 0) {
+    return 0;
+  } else {
+    numbers.reduce((prevReturnedValue, currentNum) => {
+      return currentNum + prevReturnedValue;
+    });
   }
-  return SUM;
-}; //Sum array
+};
+// const sum = (numbers) => {
+//   let SUM = 0;
+//   for (i = 0; i < numbers.length; i++) {
+//     SUM = SUM + numbers[i];
+//   }
+//   return SUM;
+// }; //Sum array
