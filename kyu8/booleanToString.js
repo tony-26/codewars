b920c7766571e7c13936a81f2816f1f066438146
Tony Wang <wangh9947@gmail.com>
2022-11-09 20:14:53 +0000

    'booleanToString' passed all tests

---
+++ kyu8/booleanToString.js
@@ -0,0 +1,9 @@
+const booleanToString = (b) => {
+  if (b) {
+    return "true";
+  } else {
+    return "false";
+  }
+};
+console.log(booleanToString(true), "true");
+console.log(booleanToString(false), "false");
