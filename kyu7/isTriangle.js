7465c3d94a28ea56dd06bd25b334d310263aafdb
Tony Wang <wangh9947@gmail.com>
2022-11-09 20:31:37 +0000

    'isTriangle' final v

--- kyu7/isTriangle.js
+++ kyu7/isTriangle.js
@@ -1,9 +1,8 @@
 const isTriangle = (a, b, c) => {
   if (a + b > c && a + c > b && b + c > a) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 console.log(isTriangle(1, 2, 2), true);
 console.log(isTriangle(7, 2, 2), false);
