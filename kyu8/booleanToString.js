926c9ddc426b0bc98697da40d0dcea16dd0b3c02
Tony Wang <wangh9947@gmail.com>
2022-11-09 20:15:26 +0000

    'booleanToString' final v

--- kyu8/booleanToString.js
+++ kyu8/booleanToString.js
@@ -1,9 +1,8 @@
 const booleanToString = (b) => {
   if (b) {
     return "true";
-  } else {
-    return "false";
   }
+  return "false";
 };
 console.log(booleanToString(true), "true");
 console.log(booleanToString(false), "false");
