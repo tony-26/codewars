81d73d4966b3158b4772b8890a5fbe12fc2ba3e7
Tony Wang <wangh9947@gmail.com>
2022-11-09 19:25:48 +0000

    'alphabetPosition' 1st att

---
+++ kyu6/alphabetPosition.js
@@ -0,0 +1,42 @@
+const alphabetPosition = (text) => {
+  let value = 0;
+  const number = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+  for (i = 0; i < text.length; i++) {
+    value = value + number[text[i]];
+  }
+};
+console.log(
+  alphabetPosition("The sunset sets at twelve o' clock."),
+  "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+);
+console.log(
+  alphabetPosition("The narwhal bacons at midnight."),
+  "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+);
