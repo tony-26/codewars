76ffa70fc637edab06ce752fd792ed5f97125772
Tony Wang <wangh9947@gmail.com>
2022-11-10 20:37:10 +0000

    'DNAtoRNA' try

---
+++ kyu8/DNAtoRNA.js
@@ -0,0 +1,6 @@
+const DNAtoRNA = (dna) => {
+  return dna.replace("T", "U");
+};
+console.log(DNAtoRNA("TTTT"), "UUUU");
+console.log(DNAtoRNA("GCAT"), "GCAU");
+console.log(DNAtoRNA("GACCGCCGCC"), "GACCGCCGCC");
