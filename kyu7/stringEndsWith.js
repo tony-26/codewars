f02b59b989c8c7fcaf607726d38ddeb04f29ee9c
Tony Wang <wangh9947@gmail.com>
2022-11-19 01:25:12 +0000

    'string end with' finished

--- kyu7/stringEndsWith.js
+++ kyu7/stringEndsWith.js
@@ -1,4 +1,14 @@
-const solution = (str, ending) => {};
-console.log(solution("abcde", "cde"));
-console.log(solution("abcde", "abc"));
+const solution = (str, ending) => {
+  let output = "";
+  if (ending === str.substring(str.length - ending.length, str.length)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(solution("abcde", "cde"), true);
+console.log(solution("abcde", "abc"), false);
 //String ends with?
+// var str = "amangupta";
+// var x = str.substring(str.length - 7, str.length);
+// console.log(x);
