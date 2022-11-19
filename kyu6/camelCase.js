998b36978b8e0dede987e3904b9e1b241ef00bef
Tony Wang <wangh9947@gmail.com>
2022-11-19 14:59:28 +0000

    'removeSmallest' nearly there

---
+++ kyu6/camelCase.js
@@ -0,0 +1,7 @@
+const solution = (string) => {
+  for (i = 0; i < string.length; i++) {
+    if (string[i].toUpperCase() === string[i]) {
+      string.charAt(i);
+    }
+  }
+};
--- kyu7/removeSmallest.js
+++ kyu7/removeSmallest.js
@@ -1,16 +1,18 @@
 const removeSmallest = (numbers) => {
+  let arr = [];
   let value = numbers[0];
   let count = 0;
   for (i = 0; i < numbers.length; i++) {
     if (numbers[i] < value) {
       value = numbers[i];
-      count += 1;
+      count = i;
     }
   }
-  numbers.slice(count, 1);
+  numbers.splice(count, 1);
   return numbers;
 };
-console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
-console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
+// console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+// console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
+//console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
 //Remove the minimum
+console.log(removeSmallest([315, 296, 88, 88, 343, 51, 130, 202]));
