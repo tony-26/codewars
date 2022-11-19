58cf760d4a320e42c5947920394338539209d76a
Tony Wang <wangh9947@gmail.com>
2022-11-19 15:50:37 +0000

    'DNA to RNA' final v

--- kyu8/DNAtoRNA.js
+++ kyu8/DNAtoRNA.js
@@ -1,13 +1,11 @@
 const DNAtoRNA = (dna) => {
   let newStr = "";
-  const change = { T: "U" };
   for (i = 0; i < dna.length; i++) {
     if (dna[i] === "T") {
       newStr = newStr + "U";
     } else {
       newStr = newStr + dna[i];
     }
-    //newStr = newStr + change[dna[i]];
   }
   return newStr;
 };
