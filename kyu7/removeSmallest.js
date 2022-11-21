16d7d859c4718bff190b1193d3f2a1229405699c
Tony Wang <wangh9947@gmail.com>
2022-11-21 09:45:54 +0000

    'removeSmallest' complete

--- kyu7/removeSmallest.js
+++ kyu7/removeSmallest.js
@@ -11,8 +11,7 @@ const removeSmallest = (numbers) => {
   numbersCopy.splice(count, 1);
   return numbersCopy;
 };
-// console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-// console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
-//console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
-//Remove the minimum
+console.log(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+console.log(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4]);
+console.log(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1]);
 console.log(removeSmallest([315, 296, 88, 88, 343, 51, 130, 202]));
