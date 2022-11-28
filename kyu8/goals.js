e261e21a807d4b712cf0bf076a02a49dc4ba0955
Tony Wang <wangh9947@gmail.com>
2022-11-28 21:21:56 +0000

    'goals' complete

---
+++ kyu8/goals.js
@@ -0,0 +1,5 @@
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+console.log(goals(0, 0, 0), 0);
+console.log(goals(43, 10, 5), 58);
