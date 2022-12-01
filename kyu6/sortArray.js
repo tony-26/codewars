71f43ac178bb440de326887d67ca86961e1aaebe
Tony Wang <wangh9947@gmail.com>
2022-12-01 22:57:25 +0000

    'sortArray' try

--- kyu6/sortArray.js
+++ kyu6/sortArray.js
@@ -1,15 +1,36 @@
+// const sortArray = (array) => {
+//   let cValue = 0;
+//   if (array.length === 0) {
+//     return array;
+//   } else {
+//     for (i = 0; i < array.length; i++) {
+//       if (array[i] % 2 !== 0) {
+//         cValue = array[i];
+//       }
+//     }
+//   }
+// };
+//assert.deepEqual(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+// assert.deepEqual(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+// assert.deepEqual(sortArray([]), []);
+
 const sortArray = (array) => {
-  let cValue = 0;
-  if (array.length === 0) {
-    return [];
-  } else {
-    for (i = 0; i < array.length; i++) {
-      if (array[i] % 2 !== 0) {
-        cValue = array[i];
+  console.log("executed");
+  const originalArry = [...array];
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] % 2 !== 0) {
+      if (array[i] > array[i + 1]) {
+        const small = array[i + 1];
+        const large = array[i];
+        array[i] = small;
+        array[i + 1] = large;
       }
     }
   }
+  if (originalArry.join() === array.join()) {
+    return array;
+  } else {
+    return sortArray(array);
+  }
 };
-assert.deepEqual(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
-assert.deepEqual(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
-assert.deepEqual(sortArray([]), []);
+console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
