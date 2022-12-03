83f1947c2af733bfbd2f8c53d22e1a2fb8d87ba9
Tony Wang <wangh9947@gmail.com>
2022-12-03 10:30:37 +0000

    'isIsogram' final version

--- kyu7/isIsogram.js
+++ kyu7/isIsogram.js
@@ -1,17 +1,14 @@
 function isIsogram(str) {
-  const chars = str.split("");
-  const sortedChars = chars.sort();
-  //console.log(sortedChars);
-  //   console.log(sortedChars.length);
-
+  const sortedChars = str.toLowerCase().split("").sort();
   for (i = 0; i < sortedChars.length; i++) {
     if (sortedChars[i] === sortedChars[i + 1]) {
       return false;
     }
-    return true;
   }
+  return true;
 }
 console.log(isIsogram("Dermatoglyphics"), true);
 console.log(isIsogram("isogram"), true);
 console.log(isIsogram("aba"), false, "same chars may not be adjacent");
 console.log(isIsogram("moOse"), false, "same chars may not be same case");
+//
