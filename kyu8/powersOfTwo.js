1ba73539010ec608a18825218c5fffde3a452702
Tony Wang <wangh9947@gmail.com>
2022-12-07 01:54:05 +0000

    'powers of two' complete

---
+++ kyu8/powersOfTwo.js
@@ -0,0 +1,10 @@
+function powersOfTwo(n) {
+  let arr = [];
+  for (let i = 0; i <= n; i++) {
+    arr.push(2 ** i);
+  }
+  return arr;
+}
+console.log(powersOfTwo(0), [1]);
+console.log(powersOfTwo(1), [1, 2]);
+console.log(powersOfTwo(4), [1, 2, 4, 8, 16]);
