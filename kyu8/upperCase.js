8fb11718839291143d3ec68483654d3629c5f215
Tony Wang <wangh9947@gmail.com>
2022-12-09 05:47:03 +0000

    'upper case'

--- kyu8/upperCase.js
+++ kyu8/upperCase.js
@@ -1,11 +1,6 @@
 String.prototype.isUpperCase = function () {
-  let str = this.toUpperCase();
-  console.log(str);
-  if (str === this) {
-    return true;
-  } else {
-    return false;
-  }
+  console.log(this);
+  return this === this.toUpperCase();
 };
 console.log("c".isUpperCase()); //false
 console.log("C".isUpperCase()); //true
