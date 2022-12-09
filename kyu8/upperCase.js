c5662778ffbebbb0901ec764ce283ad9c40bf041
Tony Wang <wangh9947@gmail.com>
2022-12-09 05:44:10 +0000

    'upperCase' try

--- kyu8/upperCase.js
+++ kyu8/upperCase.js
@@ -1,15 +1,12 @@
 String.prototype.isUpperCase = function () {
-  console.log(this);
+  let str = this.toUpperCase();
+  console.log(str);
+  if (str === this) {
+    return true;
+  } else {
+    return false;
+  }
 };
-console.log("c".isUpperCase(), false, "c is not upper case");
-console.log("C".isUpperCase(), true, "C is upper case");
-console.log(
-  "hello I AM DONALD".isUpperCase(),
-  false,
-  "hello I AM DONALD not is upper case"
-);
-console.log(
-  "HELLO I AM DONALD".isUpperCase(),
-  true,
-  "HELLO I AM DONALD is upper case"
-);
+console.log("c".isUpperCase()); //false
+console.log("C".isUpperCase()); //true
+console.log("hello I AM DONALD".isUpperCase()); //false
