9bcd33f852269dbb395304a93131d558d562677c
Tony Wang <wangh9947@gmail.com>
2022-12-20 19:43:07 +0000

    'sumStrings' 1st test passed

--- kyu4/sumStrings.js
+++ kyu4/sumStrings.js
@@ -1,41 +1,12 @@
 function sumStrings(a, b) {
-  let carry = 0;
   let currentValue = "";
-  let digit = 0;
-  let A = a.split("").reverse().join("");
-  let B = b.split("").reverse().join("");
-  //console.log(A, B);
-  // if (A.length >= B) {
-  //   for (let i = 0; i < A.length; i++) {
-  //     digit = Number(A[i]) + Number(B[i]);
-  //     if (digit >= 10) {
-  //       carry = 1;
-  //       if ((i = 0)) {
-  //         currentValue = (digit - 10).toString() + currentValue;
-  //       } else {
-  //         currentValue = (digit + carry - 10).toString() + currentValue;
-  //       }
-  //     } else if (digit < 10) {
-  //       currentValue = digit.toString() + currentValue;
-  //     }
-  //   }
-  // } else {
-  //   for (let i = 0; i < B.length; i++) {
-  //     digit = Number(A[i]) + Number(B[i]);
-  //     if (digit >= 10) {
-  //       carry = 1;
-  //       if ((i = 0)) {
-  //         currentValue = (digit - 10).toString() + currentValue;
-  //       } else {
-  //         currentValue = (digit + carry - 10).toString() + currentValue;
-  //       }
-  //     } else if (digit < 10) {
-  //       currentValue = digit.toString() + currentValue;
-  //     }
-  //   }
-  // }
+  let reverseA = a.split("").reverse();
+  let reverseB = b.split("").reverse();
+  for (let i = 0; i < reverseA.length; i++) {
+    currentValue =
+      (Number(reverseA[i]) + Number(reverseB[i])).toString() + currentValue;
+  }
+
   return currentValue;
-  // console.log(a, b);
-  // return (Number(a) + Number(b)).toString();
 }
-console.log(sumStrings("12345", "789789")); //'802134'
+console.log(sumStrings("12345", "11111")); //'23456'
