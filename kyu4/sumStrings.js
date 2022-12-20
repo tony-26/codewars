9a4b07a33a0ca4a768529dab2249e2460a6afd92
Tony Wang <wangh9947@gmail.com>
2022-12-20 20:01:24 +0000

    'sumStrings' passed 4 tests

--- kyu4/sumStrings.js
+++ kyu4/sumStrings.js
@@ -4,17 +4,19 @@ function sumStrings(a, b) {
   let reverseA = a.split("").reverse();
   let reverseB = b.split("").reverse();
   for (let i = 0; i < reverseA.length; i++) {
-    const sum = Number(reverseA[i]) + Number(reverseB[i]);
-    if (sum > 10) {
-      currentValue = (sum - 10 + carry).toString() + currentValue;
+    const sum = Number(reverseA[i]) + Number(reverseB[i]) + carry;
+    if (sum >= 10) {
+      currentValue = (sum - 10).toString() + currentValue;
       carry = 1;
     } else {
-      currentValue = (sum + carry).toString() + currentValue;
+      currentValue = sum.toString() + currentValue;
       carry = 0;
     }
   }
 
   return currentValue;
 }
-console.log(sumStrings("12345", "11111")); //'23456'
-console.log(sumStrings("12345", "11117")); //'23462'
+console.log(sumStrings("12345", "11111") === "23456");
+console.log(sumStrings("12345", "11117") === "23462");
+console.log(sumStrings("12345", "11187") === "23532");
+console.log(sumStrings("12345", "11157")); //'23502'
