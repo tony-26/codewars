bd05693db7565f93d0ce225356cc68c3a56bda42
Tony Wang <wangh9947@gmail.com>
2022-12-20 20:19:08 +0000

    'sumStrings' add eqaulLength func

--- kyu4/sumStrings.js
+++ kyu4/sumStrings.js
@@ -1,3 +1,18 @@
+const equalLengthAB = (a, b) => {
+  let newA = a;
+  let newB = b;
+  if (a.length > b.length) {
+    newB = "0".repeat(a.length - b.length) + newB;
+  }
+  if (b.length > a.length) {
+    newA = "0".repeat(b.length - a.length) + newA;
+  }
+  return [newA, newB];
+};
+console.log(equalLengthAB("123456", "9999"), ["123456", "009999"]);
+console.log(equalLengthAB("9999", "123456"), ["009999", "123456"]);
+console.log(equalLengthAB("12", "99"), ["12", "99"]);
+
 function sumStrings(a, b) {
   let carry = 0;
   let currentValue = "";
@@ -20,3 +35,6 @@ console.log(sumStrings("12345", "11111") === "23456");
 console.log(sumStrings("12345", "11117") === "23462");
 console.log(sumStrings("12345", "11187") === "23532");
 console.log(sumStrings("12345", "11157")); //'23502'
+console.log(sumStrings("123456", "176544")); //'300000'
+//console.log(sumStrings("123456", "876544")); //'1000000'
+console.log(sumStrings("123456", "9999")); //'133455'
