803b7604695716c7d718509d8b14a3629499713d
Tony Wang <wangh9947@gmail.com>
2022-12-31 17:02:34 +0000

    'sortArrays'

--- kyu6/findUniq.js
+++ kyu6/findUniq.js
@@ -1,10 +1,11 @@
 function findUniq(arr) {
-  let value = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i + 1] === value) {
-      return arr[2];
-    } else {
-      return arr[1];
-    }
-  }
+  //   let value = arr[0];
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i + 1] === value) {
+  //       return arr[2];
+  //     } else {
+  //       return arr[1];
+  //     }
+  //   }
+  let count1 = arr[0];
 }
--- kyu6/sortArray.js
+++ kyu6/sortArray.js
@@ -1,39 +1,3 @@
-// const sortArray = (array) => {
-//   let cValue = 0;
-//   if (array.length === 0) {
-//     return array;
-//   } else {
-//     for (i = 0; i < array.length; i++) {
-//       if (array[i] % 2 !== 0) {
-//         cValue = array[i];
-//       }
-//     }
-//   }
-// };
-//assert.deepEqual(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
-// assert.deepEqual(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
-// assert.deepEqual(sortArray([]), []);
-
-// const sortArray = (array) => {
-//   console.log("executed");
-//   const originalArry = [...array];
-//   for (let i = 0; i < array.length - 1; i++) {
-//     if (array[i] % 2 !== 0) {
-//       if (array[i] > array[i + 1]) {
-//         const small = array[i + 1];
-//         const large = array[i];
-//         array[i] = small;
-//         array[i + 1] = large;
-//       }
-//     }
-//   }
-//   if (originalArry.join() === array.join()) {
-//     return array;
-//   } else {
-//     return sortArray(array);
-//   }
-// };
-//console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
 const sortArray = (arr) => {
   let indexArr = [];
   //console.log("executed");
@@ -53,33 +17,6 @@ const sortArray = (arr) => {
     }
   }
   return originalArry;
-
-  //console.log(indexArr);
-  // for (let i = 0; i < arr.length; i++) {
-  //   if (currOddNum === undefined) {
-  //     if (arr[i] % 2 === 1) {
-  //       currOddNum = arr[i];
-  //       currOddIndex = i;
-  //     }
-  //   } else {
-  //     if (arr[i] % 2 === 1) {
-  //       nextOddNum = arr[i];
-  //       nextOddIndex = i;
-  //     }
-  //   }
-
-  //   if (currOddNum && nextOddNum) {
-  //     if (nextOddNum < currOddNum) {
-  //       arr[currOddIndex] = nextOddNum;
-  //       arr[nextOddIndex] = currOddNum;
-  //     }
-  //   }
-  // }
-  // if (originalArry.join() === arr.join()) {
-  //   return arr;
-  // } else {
-  //   return bsortMutate(arr);
-  // }
 };
 
 console.log(sortArray([5, 6, 1, 2, 3])); //[1, 6, 3, 2, 5]
