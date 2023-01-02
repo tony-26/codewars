8bea00bca5f346d651d36029aeeb9f5bccda0679
Tony Wang <wangh9947@gmail.com>
2023-01-02 11:17:56 +0000

    'round to next 5' try

--- kyu7/roundToNext5.js
+++ kyu7/roundToNext5.js
@@ -1,14 +1,10 @@
 function roundToNext5(n) {
   if (n % 5 === 0) {
     return n;
-  } else {
-    if (n < 5 && n > 0) {
-      return 5;
-    } else if (n < 0) {
-      return 0;
-    } else {
-      return 5 - (n % 5) + n;
-    }
+  } else if (n > 0) {
+    return n + (n % 5);
+  } else if (n < 0) {
+    return 0;
   }
 }
 console.log(roundToNext5(1));
