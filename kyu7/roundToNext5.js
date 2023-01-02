4c09d096ebf0fad4a2c2a329e4a8b65c3f854430
Tony Wang <wangh9947@gmail.com>
2023-01-02 11:23:33 +0000

    'round to next 5' passed all test

--- kyu7/roundToNext5.js
+++ kyu7/roundToNext5.js
@@ -1,10 +1,11 @@
 function roundToNext5(n) {
   if (n % 5 === 0) {
     return n;
-  } else if (n > 0) {
-    return n + (n % 5);
+  } else if (n > 0 && n % 5 !== 0) {
+    //console.log(n, n % 5);
+    return n + 5 - (n % 5);
   } else if (n < 0) {
     return 0;
   }
 }
-console.log(roundToNext5(1));
+console.log(roundToNext5(2));
