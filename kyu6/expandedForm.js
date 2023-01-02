72be5025a06cd9847e993eecbcf7855d98166895
Tony Wang <wangh9947@gmail.com>
2023-01-02 12:56:18 +0000

    'expanded form' passed all tests

--- kyu6/expandedForm.js
+++ kyu6/expandedForm.js
@@ -2,17 +2,20 @@ function expandedForm(num) {
   let str = num.toString(); //"12"
   let newStr = "";
   let finalStr = "";
+  let arr = [];
 
   for (let i = 0; i < str.length; i++) {
     if (str[i] !== "0") {
       newStr = str[i] + "0".repeat(str.length - i - 1);
-      finalStr = finalStr + newStr + " + ";
+      //finalStr = finalStr + newStr + " + ";
+      arr.push(newStr);
     }
     //console.log(str[i], newStr);
   }
+  finalStr = arr.join(" + ");
   return finalStr;
 }
 
 //console.log(expandedForm(12)); //"10 + 2"
 //console.log(expandedForm(42), "40 + 2");
-console.log(expandedForm(70304), "70000 + 300 + 4");
+console.log(expandedForm(70304) === "70000 + 300 + 4");
