d153aa54dfc1a207590770c4fc6aaa2ccd3f7930
Tony Wang <wangh9947@gmail.com>
2023-01-04 13:07:04 +0000

    'soryArray' complete

--- kyu6/sortArray.js
+++ kyu6/sortArray.js
@@ -1,6 +1,5 @@
 const sortArray = (arr) => {
   let indexArr = [];
-  //console.log("executed");
   let currOddNum, currOddIndex, nextOddNum, nextOddIndex;
   const arrCopy = [...arr];
   for (let i = 0; i < arrCopy.length; i++) {
@@ -24,6 +23,6 @@ const sortArray = (arr) => {
 };
 
 console.log(sortArray([5, 6, 1, 2, 3])); //[1, 6, 3, 2, 5]
-console.log(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+console.log(sortArray([5, 3, 2, 8, 1, 4])); //[1, 3, 2, 8, 5, 4]
 
 //console.log(sortArray([5, 6, 1, 2, 3])); // [3,2,1,4,5]
