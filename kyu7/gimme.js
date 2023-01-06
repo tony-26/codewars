8ed138a24bb6788933d73616aaea7adbc3863334
Tony Wang <wangh9947@gmail.com>
2023-01-06 16:10:07 +0000

    'gimme' right version

--- kyu7/gimme.js
+++ kyu7/gimme.js
@@ -1,20 +1,13 @@
 function gimme(triplet) {
-  let index = 0;
-  let min = 0;
-  if (triplet[0] < triplet[1]) {
-    index = 0;
-    min = triplet[0];
-  } else {
-    index = 1;
-    min = triplet[1];
+  let newArr = [...triplet];
+  newArr.sort((a, b) => a - b);
+  //console.log(newArr, triplet);
+  for (let i = 0; i < 3; i++) {
+    if (newArr[1] === triplet[i]) {
+      return i;
+    }
   }
-
-  if (triplet[2] < min) {
-    index = 2;
-    min = triplet[2];
-  }
-  return index;
 }
 console.log(gimme([2, 3, 1], 0));
-console.log(gimme([2.1, 3.2, 1.4], 0));
-console.log(gimme([-2, -3.2, 1], 0));
+// console.log(gimme([2.1, 3.2, 1.4], 0));
+// console.log(gimme([-2, -3.2, 1], 0));
