cb8e566720e4a34a645d2e88b30f2fa3969d188c
Tony Wang <wangh9947@gmail.com>
2023-01-10 17:41:32 +0000

    'calcualteYears' complete

--- kyu6/findUniq.js
+++ kyu6/findUniq.js
@@ -1,14 +1,14 @@
 function findUniq(arr) {
   let uniqueNum = arr[0];
   for (let i = 1; i < arr.length; i++) {
-    // if (arr[i] !== uniqueNum) {
-    //   return arr[i];
-    // }
-    if (arr[i] !== arr[i + 1] && arr[i] !== arr[i - 1]) {
+    if (arr[i] !== uniqueNum) {
       return arr[i];
     }
+    // if (arr[i] !== arr[i + 1] && arr[i] !== arr[i - 1]) {
+    //   return arr[i];
+    // }
   }
-  //return uniqueNum;
+  return uniqueNum;
 }
 console.log(findUniq([1, 0, 0]), 1);
 console.log(findUniq([0, 1, 0]), 1);
--- kyu7/calculateYears.js
+++ kyu7/calculateYears.js
@@ -1,1 +1,11 @@
-function calculateYears(principal, interest, tax, desired) {}
+function calculateYears(principal, interest, tax, desired) {
+  let year = 0;
+  while (principal < desired) {
+    principal += principal * interest * (1 - tax);
+    year += 1;
+  }
+  return year;
+}
+assert.strictEqual(calculateYears(1000, 0.05, 0.18, 1100), 3);
+assert.strictEqual(calculateYears(1000, 0.01625, 0.18, 1200), 14);
+assert.strictEqual(calculateYears(1000, 0.05, 0.18, 1000), 0);
