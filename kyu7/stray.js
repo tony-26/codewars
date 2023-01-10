6451742d72933221476d34b3fb0da9ee10cb5be8
Tony Wang <wangh9947@gmail.com>
2023-01-10 16:57:27 +0000

    'stray' complete

--- kyu7/stray.js
+++ kyu7/stray.js
@@ -1,8 +1,9 @@
 const stray = (numbers) => {
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] !== numbers[i + 1] && numbers[i] !== numbers[i - 1]) {
-      return numbers[i];
-    }
-  }
+  // for (let i = 0; i < numbers.length; i++) {
+  //   if (numbers[i] !== numbers[i + 1] && numbers[i] !== numbers[i - 1]) {
+  //     return numbers[i];
+  //   }
+  // }
+  return numbers.reduce((a, b) => a ^ b);
 };
 console.log(stray([1, 1, 2]), 2);
