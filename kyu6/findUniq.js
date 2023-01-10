14d11b209042fa7090c00c49ba1a30237e2b4b2b
Tony Wang <wangh9947@gmail.com>
2023-01-10 17:35:11 +0000

    'findUniq' second att

--- kyu6/findUniq.js
+++ kyu6/findUniq.js
@@ -1,11 +1,14 @@
 function findUniq(arr) {
   let uniqueNum = arr[0];
   for (let i = 1; i < arr.length; i++) {
-    if (arr[i] !== uniqueNum) {
+    // if (arr[i] !== uniqueNum) {
+    //   return arr[i];
+    // }
+    if (arr[i] !== arr[i + 1] && arr[i] !== arr[i - 1]) {
       return arr[i];
     }
   }
-  return unique;
+  //return uniqueNum;
 }
 console.log(findUniq([1, 0, 0]), 1);
 console.log(findUniq([0, 1, 0]), 1);
