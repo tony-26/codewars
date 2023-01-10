fdfa6c98053e00fc5691a9ed0ece379597fb5edf
Tony Wang <wangh9947@gmail.com>
2023-01-10 17:18:17 +0000

    'findUniq' passed tests

--- kyu6/findUniq.js
+++ kyu6/findUniq.js
@@ -1,11 +1,15 @@
 function findUniq(arr) {
-  //   let value = arr[0];
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if (arr[i + 1] === value) {
-  //       return arr[2];
-  //     } else {
-  //       return arr[1];
-  //     }
-  //   }
-  let count1 = arr[0];
+  let uniqueNum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== uniqueNum) {
+      return arr[i];
+    }
+  }
+  return unique;
 }
+console.log(findUniq([1, 0, 0]), 1);
+console.log(findUniq([0, 1, 0]), 1);
+console.log(findUniq([0, 0, 1]), 1);
+console.log(findUniq([1, 1, 1, 2, 1, 1]), 2);
+console.log(findUniq([1, 1, 2, 1, 1]), 2);
+console.log(findUniq([3, 10, 3, 3, 3]), 10);
