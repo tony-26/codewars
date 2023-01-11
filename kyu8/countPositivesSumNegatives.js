986aee4180f8e44eb0538ca247133ea1d2098fc0
Tony Wang <wangh9947@gmail.com>
2023-01-11 19:35:58 +0000

    'countPositive..

---
+++ kyu8/countPositivesSumNegatives.js
@@ -0,0 +1,16 @@
+function countPositivesSumNegatives(input) {
+  if (input.length === 0) {
+    return [];
+  } else {
+    let positive = 0;
+    let negtive = 0;
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] > 0) {
+        positive = positive + 1;
+      } else if (input[i] < 0) {
+        negtive = negtive + input[i];
+      }
+    }
+    return [positive, negtive];
+  }
+}
