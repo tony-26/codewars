8d84ac661d880cf36d2174d28811b761f68bb07a
Tony Wang <wangh9947@gmail.com>
2023-01-11 19:18:47 +0000

    'count' try

--- kyu6/count.js
+++ kyu6/count.js
@@ -1,8 +1,9 @@
 function count(string) {
   if (string.length === 0) {
-    return string;
+    return {};
   } else {
+    for (let i = 0; i < string.length; i++) {}
   }
 }
 console.log(count("aba"), { a: 2, b: 1 });
-console.log(count(""), {});
+//console.log(count(""), {});
