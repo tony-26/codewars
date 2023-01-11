3af15d2ae1e4736715bc70aac5580172d1453ef3
Tony Wang <wangh9947@gmail.com>
2023-01-11 18:59:31 +0000

    'accum' complete

---
+++ kyu7/accum.js
@@ -0,0 +1,27 @@
+function accum(s) {
+  let result = [];
+  for (let i = 0; i < s.length; i++) {
+    result.push(s[i].toUpperCase() + s[i].toLowerCase().repeat(i));
+  }
+  return result.join("-");
+}
+console.log(
+  accum("ZpglnRxqenU"),
+  "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+);
+console.log(
+  accum("NyffsGeyylB"),
+  "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+);
+console.log(
+  accum("MjtkuBovqrU"),
+  "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+);
+console.log(
+  accum("EvidjUnokmM"),
+  "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+);
+console.log(
+  accum("HbideVbxncC"),
+  "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+);
