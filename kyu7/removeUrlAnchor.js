116dbd8fa04095090226ab1e59094b0438a27326
Tony Wang <wangh9947@gmail.com>
2023-01-12 17:26:30 +0000

    'remove...' final version

--- kyu7/removeUrlAnchor.js
+++ kyu7/removeUrlAnchor.js
@@ -1,19 +1,12 @@
 function removeUrlAnchor(url) {
-  //   let index = 0;
-  //   for (let i = 0; i < url.length; i++) {
-  //     if (url[i] === "#") {
-  //       index = i;
-  //     }
-  //   }
-  //   return url.slice(i);
   return url.split("#")[0];
 }
 console.log(removeUrlAnchor("www.codewars.com#about"), "www.codewars.com");
-// console.log(
-//   removeUrlAnchor("www.codewars.com/katas/?page=1#about"),
-//   "www.codewars.com/katas/?page=1"
-// );
-// console.log(
-//   removeUrlAnchor("www.codewars.com/katas/"),
-//   "www.codewars.com/katas/"
-// );
+console.log(
+  removeUrlAnchor("www.codewars.com/katas/?page=1#about"),
+  "www.codewars.com/katas/?page=1"
+);
+console.log(
+  removeUrlAnchor("www.codewars.com/katas/"),
+  "www.codewars.com/katas/"
+);
