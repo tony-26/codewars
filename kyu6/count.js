397d560dd68cbcf315a6bdee2e08bf27a41968eb
Tony Wang <wangh9947@gmail.com>
2023-01-14 10:43:39 +0000

    'count' (object) complete

--- kyu6/count.js
+++ kyu6/count.js
@@ -1,9 +1,17 @@
 function count(string) {
   if (string.length === 0) {
     return {};
-  } else {
-    for (let i = 0; i < string.length; i++) {}
   }
+  let obj = {};
+  let arr = string.split("");
+  arr.forEach((e) => {
+    if (obj[e] === undefined) {
+      obj[e] = 1;
+    } else {
+      obj[e] += 1;
+    }
+  });
+  return obj;
 }
 console.log(count("aba"), { a: 2, b: 1 });
 //console.log(count(""), {});
