eae3a40aa8699e3a5f5b9c8ecae4b6e12d2a3d8b
Tony Wang <wangh9947@gmail.com>
2023-01-14 10:57:12 +0000

    'duplicateEncode'

--- kyu6/duplicateEncode.js
+++ kyu6/duplicateEncode.js
@@ -1,9 +1,20 @@
 function duplicateEncode(word) {
-    for (let i = 0;i<word.length;i++){
-        if (word[i])
+  let split = word.toLowerCase().split("");
+  let firstProcessed = [];
+  let indexOfFirstDuplicate = [];
+  let result = "";
+
+  for (let i = 0; i < word.length; i++) {
+    if (process.includes(split[i])) {
+      result += ")";
+    } else {
+      result += "(";
     }
+    process.push(split[i]);
+  }
+  return result;
 }
 console.log(duplicateEncode("din"), "(((");
-// console.log(duplicateEncode("recede"), "()()()");
+console.log(duplicateEncode("recede"), "()()()");
 // console.log(duplicateEncode("Success"), ")())())", "should ignore case");
 // console.log(duplicateEncode("(( @"), "))((");
