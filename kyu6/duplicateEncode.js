e6e786cbee8d42cb98fea92785faf1e8881b80fb
Tony Wang <wangh9947@gmail.com>
2023-01-14 00:34:47 +0000

    'dup' try

---
+++ kyu6/duplicateEncode.js
@@ -0,0 +1,9 @@
+function duplicateEncode(word) {
+    for (let i = 0;i<word.length;i++){
+        if (word[i])
+    }
+}
+console.log(duplicateEncode("din"), "(((");
+// console.log(duplicateEncode("recede"), "()()()");
+// console.log(duplicateEncode("Success"), ")())())", "should ignore case");
+// console.log(duplicateEncode("(( @"), "))((");
