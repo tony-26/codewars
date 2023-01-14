b6976ba052d42f434ac6eb2c74cfc4602c114f15
Tony Wang <wangh9947@gmail.com>
2023-01-14 10:25:05 +0000

    'toCamelCase' complete

--- kyu6/toCamelCase.js
+++ kyu6/toCamelCase.js
@@ -1,37 +1,25 @@
+const splitBy = (str) => {
+  let splitArr = [];
+  str.split("_").forEach((e) => {
+    splitArr.push(...e.split("-"));
+  });
+
+  return splitArr;
+};
+//console.log(splitBy("hello_Tony-hi-good")); //["hello", "Tony", "hi", "good"]
+
 function toCamelCase(str) {
   if (str.length === 0) {
     return "";
-  } else if (str.includes("-")) {
-    let arr = str.split("-");
-    for (let i = 1; i < arr.length; i++) {
-      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-    }
-    return arr.join("");
-  } else if (str.includes("_")) {
-    let arr = str.split("_");
+  } else {
+    let arr = splitBy(str);
     for (let i = 1; i < arr.length; i++) {
       arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
     }
     return arr.join("");
   }
 }
-console.log(
-  toCamelCase(""),
-  "",
-  "An empty string was provided but not returned"
-);
-console.log(
-  toCamelCase("the_stealth_warrior"),
-  "theStealthWarrior",
-  "toCamelCase('the_stealth_warrior') did not return correct value"
-);
-console.log(
-  toCamelCase("The-Stealth-Warrior"),
-  "TheStealthWarrior",
-  "toCamelCase('The-Stealth-Warrior') did not return correct value"
-);
-console.log(
-  toCamelCase("A-B-C"),
-  "ABC",
-  "toCamelCase('A-B-C') did not return correct value"
-);
+console.log(toCamelCase(""), "");
+console.log(toCamelCase("the_stealth_warrior"), "theStealthWarrior");
+console.log(toCamelCase("The-Stealth-Warrior"), "TheStealthWarrior");
+console.log(toCamelCase("A-B-C"), "ABC");
