c93bce202943e07382cd7ef2ac653665518d792b
Tony Wang <wangh9947@gmail.com>
2023-01-17 19:22:15 +0000

    'high' try

---
+++ kyu6/high.js
@@ -0,0 +1,8 @@
+function high(x) {
+  let arr = x.split(" ");
+}
+console.log(high("man i need a taxi up to ubud"), "taxi");
+console.log(high("what time are we climbing up the volcano"), "volcano");
+console.log(high("take me to semynak"), "semynak");
+console.log(high("aa b"), "aa");
+console.log(high("b aa"), "b");
