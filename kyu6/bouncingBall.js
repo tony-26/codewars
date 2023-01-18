9b48913619a36c0cf8fafd06b4a2eaa9d782989e
Tony Wang <wangh9947@gmail.com>
2023-01-18 17:54:48 +0000

    'bounceBall' att

---
+++ kyu6/bouncingBall.js
@@ -0,0 +1,15 @@
+function bouncingBall(h, bounce, window) {
+  if (h <= 0) {
+    return -1;
+  } else if (bounce <= 0 || bounce >= 1) {
+    return -1;
+  } else if (window >= h) {
+    return -1;
+  }
+  let bounceTimes = 0;
+  while (h > window) {
+    bounceTimes += 2;
+    h = h * bounce;
+  }
+  return bounceTimes;
+}
