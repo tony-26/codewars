ae4e265c5b6494069760bdfe2c26d17533e06d01
Tony Wang <wangh9947@gmail.com>
2023-01-18 17:04:27 +0000

    'wilson' complete

--- kyu8/amIWilson.js
+++ kyu8/amIWilson.js
@@ -1,14 +1,25 @@
-const amIWilson = (p) => {
-  let product = 1;
-  for (i = p - 1; i >= 1; i--) {
-    product = product * i;
-  }
-  if ((product + 1) % (p * p) === 0) {
-    return true;
-  } else {
+// const amIWilson = (p) => {
+//   // let product = 1;
+//   // for (i = p - 1; i >= 1; i--) {
+//   //   product = product * i;
+//   // }
+//   // if ((product + 1) % (p * p) === 0) {
+//   //   return true;
+//   // } else {
+//   //   return false;
+//   // }
+
+// };
+function isWilsonPrime(num) {
+  if (num < 5) {
     return false;
   }
-};
+  let factorial = 1;
+  for (let i = 2; i < num; i++) {
+    factorial *= i;
+  }
+  return (factorial + 1) % (num * num) === 0;
+}
 // console.log(amIWilson(5)); // true
 console.log(amIWilson(9)); // false
 console.log(amIWilson(563));
