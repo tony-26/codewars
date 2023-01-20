0e604077c561abcf66d3a08c26e00d2941cdeaf2
Tony Wang <wangh9947@gmail.com>
2023-01-20 22:13:44 +0000

    'capitals' complete

---
+++ kyu7/capitals.js
@@ -0,0 +1,10 @@
+var capitals = function (word) {
+  let result = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
+      result.push(i);
+    }
+  }
+  return result;
+};
+console.log(capitals("CodEWaRs"), [0, 3, 4, 6]);
