857bac2f73ca1e07003a59939987164829be070b
Tony Wang <wangh9947@gmail.com>
2023-01-21 09:43:00 +0000

    'countSmiley'

--- kyu6/countSmileys.js
+++ kyu6/countSmileys.js
@@ -1,12 +1,10 @@
 function countSmileys(arr) {
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (
-      arr[i].includes(";") ||
-      (arr[i].includes(":") && arr[i].includes("D")) ||
-      arr[i].includes(")")
-    ) {
-      count += 1;
+    if (arr[i].includes(";") || arr[i].includes(":")) {
+      if (arr[i].includes("D") || arr[i].includes(")")) {
+        count += 1;
+      }
     }
   }
   return count;
