45f4d82d6f1486ad205a91810ed3910a811809b0
Tony Wang <wangh9947@gmail.com>
2023-01-21 10:16:36 +0000

    'countSmiley' passed all tests

--- kyu6/countSmileys.js
+++ kyu6/countSmileys.js
@@ -3,13 +3,19 @@ function countSmileys(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].includes(";") || arr[i].includes(":")) {
       if (arr[i].includes("D") || arr[i].includes(")")) {
-        count += 1;
+        if (arr[i].length === 2) {
+          count += 1;
+        }
+        if (arr[i].length === 3 && ["-", "~"].includes(arr[i][1])) {
+          count += 1;
+        }
       }
     }
   }
   return count;
 }
-console.log(countSmileys([]), 0);
-console.log(countSmileys([":D", ":~)", ";~D", ":)"]), 4);
-console.log(countSmileys([":)", ":(", ":D", ":O", ":;"]), 2);
-console.log(countSmileys([";]", ":[", ";*", ":$", ";-D"]), 1);
+// console.log(countSmileys([]), 0);
+// console.log(countSmileys([":D", ":~)", ";~D", ":)"]), 4);
+// console.log(countSmileys([":)", ":(", ":D", ":O", ":;"]), 2);
+// console.log(countSmileys([";]", ":[", ";*", ":$", ";-D"]), 1);
+console.log(countSmileys([":-)", ";~D", ":-D", ":_D"]));
