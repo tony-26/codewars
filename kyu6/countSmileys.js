5ba83f1bc3bf35f8e732c6e3cb63eefa494101e2
Tony Wang <wangh9947@gmail.com>
2023-01-21 10:24:19 +0000

    'countSmilley' 2 if statements

--- kyu6/countSmileys.js
+++ kyu6/countSmileys.js
@@ -1,15 +1,24 @@
 function countSmileys(arr) {
+  const validEyes = [";", ":"];
+  const validNoses = ["-", "~"];
+  const validFaces = ["D", ")"];
+
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i].includes(";") || arr[i].includes(":")) {
-      if (arr[i].includes("D") || arr[i].includes(")")) {
-        if (arr[i].length === 2) {
-          count += 1;
-        }
-        if (arr[i].length === 3 && ["-", "~"].includes(arr[i][1])) {
-          count += 1;
-        }
-      }
+    if (
+      arr[i].length === 3 &&
+      validEyes.includes(arr[i][0]) &&
+      validNoses.includes(arr[i][1]) &&
+      validFaces.includes(arr[i][2])
+    ) {
+      count += 1;
+    }
+    if (
+      arr[i].length === 2 &&
+      validEyes.includes(arr[i][0]) &&
+      validFaces.includes(arr[i][1])
+    ) {
+      count += 1;
     }
   }
   return count;
