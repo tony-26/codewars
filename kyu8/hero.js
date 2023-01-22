0148325b88c2b868ff051f2732684dc204dc1fbb
Tony Wang <wangh9947@gmail.com>
2023-01-22 11:28:14 +0000

    'hero' complete

---
+++ kyu8/hero.js
@@ -0,0 +1,13 @@
+function hero(bullets, dragons) {
+  if (bullets >= dragons * 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(hero(10, 5), true);
+console.log(hero(7, 4), false);
+console.log(hero(4, 5), false);
+console.log(hero(100, 40), true);
+console.log(hero(1500, 751), false);
+console.log(hero(0, 1), false);
