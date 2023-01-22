e186f0a1566ce2ae27c647c5a77e317418b8d7b1
Tony Wang <wangh9947@gmail.com>
2023-01-22 11:00:05 +0000

    'diamond' try

--- kyu6/diamond.js
+++ kyu6/diamond.js
@@ -2,11 +2,16 @@ function diamond(n) {
   if (n % 2 === 0 || n < 0) {
     return null;
   }
+  let stars = "*".repeat()
   const results = [];
   for (let i = 1; i <= 3; i++) {
     const stars = "*".repeat(2 * i - 1);
     const pluses = "+".repeat(3 - i);
     results.push(pluses + stars + "\n");
+  }
+  for (let i = 3; i>=1;i--){
+    
+    const decreaseStars
   }
   return results.join("");
 }
