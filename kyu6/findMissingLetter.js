dc57c923639de4d68501318365feac5e7838d34b
Tony Wang <wangh9947@gmail.com>
2023-01-27 17:36:35 +0000

    'findMissingLetter' found missing letter

--- kyu6/findMissingLetter.js
+++ kyu6/findMissingLetter.js
@@ -1,7 +1,9 @@
 function findMissingLetter(array) {
-  for (let i = 0; i < array.length - 1; i++) {}
-
-  return " ";
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i + 1].charCodeAt() - array[i].charCodeAt() > 1) {
+      return array[i + 1];
+    }
+  }
 }
 console.log(findMissingLetter(["a", "b", "c", "d", "f"]), "e");
 console.log(findMissingLetter(["O", "Q", "R", "S"]), "P");
