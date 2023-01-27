65e4f9eb530b9241d0aba85cc84ad01a7c8823db
Tony Wang <wangh9947@gmail.com>
2023-01-27 17:29:59 +0000

    'findMissingLetter' try

---
+++ kyu6/findMissingLetter.js
@@ -0,0 +1,7 @@
+function findMissingLetter(array) {
+  for (let i = 0; i < array.length - 1; i++) {}
+
+  return " ";
+}
+console.log(findMissingLetter(["a", "b", "c", "d", "f"]), "e");
+console.log(findMissingLetter(["O", "Q", "R", "S"]), "P");
