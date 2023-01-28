5efcb98e331d1b927664a7e85a4c16f39d712264
Tony Wang <wangh9947@gmail.com>
2023-01-28 22:57:11 +0000

    'isValidWalk'

--- kyu6/isValidWalk.js
+++ kyu6/isValidWalk.js
@@ -2,6 +2,24 @@ function isValidWalk(walk) {
   if (walk.length !== 10) {
     return false;
   }
+  let leftAndRight = 0;
+  let upAndDown = 0;
+  for (let i = 0; i < walk.length; i++) {
+    if (walk[i] === "n") {
+      upAndDown++;
+    } else if (walk[i] === "s") {
+      upAndDown--;
+    } else if (walk[i] === "w") {
+      leftAndRight--;
+    } else if (walk[i] === "e") {
+      leftAndRight++;
+    }
+  }
+  if (leftAndRight === 0 && upAndDown === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 console.log(
   isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]),
