3dc62ec48f2aa703ff69640b6fdbc84dd4059c7b
Tony Wang <wangh9947@gmail.com>
2023-01-29 19:59:09 +0000

    'problem' complete

---
+++ kyu8/problem.js
@@ -0,0 +1,17 @@
+function problem(x) {
+  if (typeof x === "string") {
+    return "Error";
+  } else {
+    return x * 50 + 6;
+  }
+}
+console.log(problem("hello"), "Error");
+console.log(problem(1), 56);
+console.log(problem(5), 256);
+console.log(problem(0), 6);
+console.log(problem(1.2), 66);
+console.log(problem(3), 156);
+console.log(problem("RyanIsCool"), "Error");
+console.log(problem(-3), -144);
+console.log(problem(""), "Error");
+console.log(problem(0.03), 7.5);
