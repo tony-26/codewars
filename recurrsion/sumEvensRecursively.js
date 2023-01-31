529d64a392e4c7962781f6795dfe6da1efe56163
Tony Wang <wangh9947@gmail.com>
2023-01-31 20:04:32 +0000

    'sumEvensRecursively' Refactored

--- recurrsion/sumEvensRecursively.js
+++ recurrsion/sumEvensRecursively.js
@@ -1,14 +1,12 @@
 const sumEvensRecursively = (arr, sumEven = 0) => {
-  if (arr.length === 0) {
-    return sumEven;
-  } else {
-    if (arr[0] % 2 === 0) {
-      sumEven = sumEven + arr[0];
-      arr.shift();
-    } else if (arr[0] % 2 !== 0) {
-      arr.shift();
-    }
-    return sumEvensRecursively(arr, sumEven);
+  //Condition for termination
+  if (arr.length === 0) return sumEven;
+  //Recursive step
+  const currentElement = arr.shift();
+  if (currentElement % 2 === 0) {
+    sumEven = sumEven + currentElement;
   }
+  return sumEvensRecursively(arr, sumEven);
 };
+
 console.log(sumEvensRecursively([1, 2, 3, 4, 5])); //6
