08aa2e70e081332290bfbbbd8898a76a682b46dc
Tony Wang <wangh9947@gmail.com>
2023-02-02 21:16:55 +0000

    'isAnagram' try

--- kyu7/isAnagram.js
+++ kyu7/isAnagram.js
@@ -2,9 +2,16 @@ var isAnagram = function (test, original) {
   if (test.length !== original) {
     return false;
   }
-  if (test.includes(original)) {
+  let sameCaseTest = test.toLowerCase();
+  let sameCaseOriginal = original.toLowerCase();
+  let sortedTest = sameCaseTest.split("").sort().join("");
+  let sortedOriginal = sameCaseOriginal.split("").sort().join("");
+  if (sortedTest === sortedOriginal) {
     return true;
   } else {
     return false;
   }
 };
+console.log(isAnagram("foefet", "toffee"), true);
+console.log(isAnagram("Buckethead", "DeathCubeK"), true);
+console.log(isAnagram("Twoo", "WooT"), true);
