52f83fda81bb83b1e66c9db4c6e9b2fc5bc91e0d
Tony Wang <wangh9947@gmail.com>
2023-02-03 16:36:40 +0000

    'stockList' try

---
+++ kyu6/stockList.js
@@ -0,0 +1,9 @@
+function stockList(listOfArt, listOfCat) {
+  let finalData = "";
+  for (let i = 0; i < listOfArt.length; i++) {}
+}
+let b, c, res;
+b = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"];
+c = ["A", "B", "C", "D"];
+res = "(A : 0) - (B : 1290) - (C : 515) - (D : 600)";
+console.log(stockList(b, c), res);
