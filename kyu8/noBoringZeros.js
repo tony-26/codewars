3defabfb288c164b68a5a71c3dd51f9fb0b547c1
Tony Wang <wangh9947@gmail.com>
2023-02-03 23:11:21 +0000

    'noBoringZeros' complete

--- kyu8/noBoringZeros.js
+++ kyu8/noBoringZeros.js
@@ -1,12 +1,25 @@
 function noBoringZeros(n) {
-  if (n.toString()[n.toString().length] !== 0) {
-    return n;
-  }
+  let str = n.toString();
   if (n === 0) {
     return 0;
   }
-  let arr = n.toString().split("").reverse();
-  for (let i = 0; i < arr.length; i++) {}
+  //   if (n.toString()[n.toString().length] !== 0) {
+  //     return n;
+  //   }
+  //   if (n === 0) {
+  //     return 0;
+  //   }
+  //   let arr = n.toString().split("").reverse();
+  //   for (let i = 0; i < arr.length; i++) {}
+  if (str[str.length - 1] !== "0") {
+    return str;
+  } else {
+    const arr = str.split("");
+    arr.pop();
+    return noBoringZeros(Number(arr.join("")));
+    // const newStr = str.split("").pop().join("");
+    //return noBoringZeros(Number(newStr));
+  }
 }
 console.log(noBoringZeros(1450), 145);
 console.log(noBoringZeros(960000), 96);
@@ -14,4 +27,3 @@ console.log(noBoringZeros(1050), 105);
 console.log(noBoringZeros(-1050), -105);
 console.log(noBoringZeros(-105), -105);
 console.log(noBoringZeros(0), 0);
-//[0, 5, 4, 1]
