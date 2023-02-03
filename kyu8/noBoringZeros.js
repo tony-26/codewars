75a3317ea195bf7d7bc373e280c99ee914886367
Tony Wang <wangh9947@gmail.com>
2023-02-03 23:13:28 +0000

    'no boring zeros' final version

--- kyu8/noBoringZeros.js
+++ kyu8/noBoringZeros.js
@@ -3,22 +3,12 @@ function noBoringZeros(n) {
   if (n === 0) {
     return 0;
   }
-  //   if (n.toString()[n.toString().length] !== 0) {
-  //     return n;
-  //   }
-  //   if (n === 0) {
-  //     return 0;
-  //   }
-  //   let arr = n.toString().split("").reverse();
-  //   for (let i = 0; i < arr.length; i++) {}
   if (str[str.length - 1] !== "0") {
     return str;
   } else {
     const arr = str.split("");
     arr.pop();
     return noBoringZeros(Number(arr.join("")));
-    // const newStr = str.split("").pop().join("");
-    //return noBoringZeros(Number(newStr));
   }
 }
 console.log(noBoringZeros(1450), 145);
