5114cc37a9728ba4d9eef0060ab994a6f171478e
Tony Wang <wangh9947@gmail.com>
2023-02-04 10:57:51 +0000

    'base10 to base2' complete

--- recurrsion/base10ToBase2Converter.js
+++ recurrsion/base10ToBase2Converter.js
@@ -1,13 +1,17 @@
 const base10ToBase2Converter = (n, result = 0) => {
-  let i = 1;
+  if (n === 0) {
+    return result;
+  }
+  let i = 0;
   while (true) {
     if (2 ** i > n) {
       n = n - 2 ** (i - 1);
-      result = 10 ** (i - 1);
+      result = result + 10 ** (i - 1);
+      //console.log({ n, result });
       return base10ToBase2Converter(n, result);
     } else {
       i += 1;
     }
   }
 };
-console.log(base10ToBase2Converter(36)); //1100100
+console.log(base10ToBase2Converter(103)); //1100100
