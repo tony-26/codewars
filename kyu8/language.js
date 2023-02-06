afe37bcab8884cb8e10d98ced3aaea85d5731974
Tony Wang <wangh9947@gmail.com>
2023-02-06 19:35:16 +0000

    'language'

---
+++ kyu8/language.js
@@ -0,0 +1,29 @@
+function greet(language) {
+  const languages = {
+    english: "Welcome",
+    czech: "Vitejte",
+    danish: "Velkomst",
+    dutch: "Welkom",
+    estonian: "Tere tulemast",
+    finnish: "Tervetuloa",
+    flemish: "Welgekomen",
+    french: "Bienvenue",
+    german: "Willkommen",
+    irish: "Failte",
+    italian: "Benvenuto",
+    latvian: "Gaidits",
+    lithuanian: "Laukiamas",
+    polish: "Witamy",
+    spanish: "Bienvenido",
+    swedish: "Valkommen",
+    welsh: "Croeso",
+  };
+  if (
+    language === "IP_ADDRESS_INVALID" ||
+    language === "IP_ADDRESS_NOT_FOUND" ||
+    language === "IP_ADDRESS_REQUIRED"
+  ) {
+    return "Welcome";
+  }
+  return languages[language];
+}
