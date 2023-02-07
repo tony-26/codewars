ddabfb228e0a20e983f036477092d8855e842423
Tony Wang <wangh9947@gmail.com>
2023-02-07 19:52:29 +0000

    'baseN to baseM' conver

--- recurrsion/baseNtoBaseMConverter.js
+++ recurrsion/baseNtoBaseMConverter.js
@@ -1,26 +1,29 @@
-const baseNtoBaseMConverter = (num, n, m) => {
+const baseNToBase10 = (num, baseN) => {
   let reverse = num.toString().split("").reverse();
   let decimalN = 0;
-  //console.log(reverse);
-  for (let i = 0; i < n.toString().length; i++) {
-    decimalN = decimalN + Number(reverse[i]) * n ** i;
-  }
-  const base10ToBaseNConverter = (decimalN, m, result = 0) => {
-    if (num === 0) {
-      return result;
-    }
-    let i = 0;
-    while (true) {
-      if (m ** i > num) {
-        num = num - m ** (i - 1);
-        result = result + 10 ** (i - 1);
-        console.log({ num, result });
-        return base10ToBaseNConverter(num, m, result);
-      } else {
-        i += 1;
-      }
-    }
-  };
-};
 
-console.log(baseNtoBaseMConverter(101, 2, 10)); //5
+  for (let i = 0; i < num.toString().length; i++) {
+    decimalN = decimalN + Number(reverse[i]) * baseN ** i;
+  }
+
+  return decimalN;
+};
+const base10ToBaseN = (num, n, result = 0) => {
+  if (num === 0) {
+    return result;
+  }
+  let i = 0;
+  while (true) {
+    if (n ** i > num) {
+      num = num - n ** (i - 1);
+      result = result + 10 ** (i - 1);
+      return base10ToBaseN(num, n, result);
+    } else {
+      i += 1;
+    }
+  }
+};
+const baseNtoBaseM = (num, baseN, baseM) =>
+  base10ToBaseN(baseNToBase10(num, baseN), baseM);
+console.log(baseNtoBaseM(101, 2, 10)); //5
+console.log(baseNtoBaseM(4321, 5, 6)); //2414
