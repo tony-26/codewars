c45522cce7e30c71750f74268a9aac8a0e5a3a88
Tony Wang <wangh9947@gmail.com>
2023-02-07 16:37:13 +0000

    'baseNtoBaseMconv' try

--- recurrsion/baseNtoBaseMConverter.js
+++ recurrsion/baseNtoBaseMConverter.js
@@ -1,12 +1,26 @@
-const baseNtoBaseMConverter = (num, n, m, result = 0) => {
-  if (num === 0) {
-    return result;
-  }
-  let reverse = n.toString().reverse();
+const baseNtoBaseMConverter = (num, n, m) => {
+  let reverse = num.toString().split("").reverse();
   let decimalN = 0;
-  console.log(reverse);
+  //console.log(reverse);
   for (let i = 0; i < n.toString().length; i++) {
     decimalN = decimalN + Number(reverse[i]) * n ** i;
   }
+  const base10ToBaseNConverter = (decimalN, m, result = 0) => {
+    if (num === 0) {
+      return result;
+    }
+    let i = 0;
+    while (true) {
+      if (m ** i > num) {
+        num = num - m ** (i - 1);
+        result = result + 10 ** (i - 1);
+        console.log({ num, result });
+        return base10ToBaseNConverter(num, m, result);
+      } else {
+        i += 1;
+      }
+    }
+  };
 };
-console.log(baseNtoBaseMConverter(101, 2, 10, result)); //5
+
+console.log(baseNtoBaseMConverter(101, 2, 10)); //5
