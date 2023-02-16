f90c34e8830b57520f233d506556149aa89e6a5c
Tony Wang <wangh9947@gmail.com>
2023-02-16 12:14:05 +0000

    fixed mistakes

--- kyu6/isPrime.js
+++ kyu6/isPrime.js
@@ -2,6 +2,12 @@ function isPrime(num) {
   if (num <= 1) {
     return false;
   }
+  if (num === 2) {
+    return true;
+  }
+  if (num % 2 === 0) {
+    return false;
+  }
   for (let i = 3; i <= Math.sqrt(num); i += 2) {
     if (num % i === 0) {
       return false;
