ed6b5f49e4b124169b0ed1261ce1f7c9a937802b
Tony Wang <wangh9947@gmail.com>
2023-02-17 20:29:26 +0000

    minutes to hours working

--- kyu4/formatDuration.js
+++ kyu4/formatDuration.js
@@ -25,9 +25,29 @@ const secondsToMinutes = (seconds) => {
     }
   }
 };
-console.log(secondsToMinutes(70)); // "1 minute 10 seconds"
-console.log(secondsToMinutes(120)); // "2 minutes 0 seconds"
-console.log(secondsToMinutes(3661)); // "61 minutes 1 second"
+
+const minutesToHours = (minutes) => {
+  let hours = Math.floor(minutes / 60);
+  let remainingMinutes = minutes % 60;
+
+  if (hours === 1) {
+    if (remainingMinutes === 1) {
+      return hours + " hour " + remainingMinutes + " minute";
+    } else {
+      return hours + " hour " + remainingMinutes + " minutes";
+    }
+  } else {
+    if (remainingMinutes === 1) {
+      return hours + " hours " + remainingMinutes + " minute";
+    } else {
+      return minutes + " hours " + remainingMinutes + " minutes";
+    }
+  }
+};
+console.log(minutesToHours(70));
+// console.log(secondsToMinutes(70)); // "1 minute 10 seconds"
+// console.log(secondsToMinutes(120)); // "2 minutes 0 seconds"
+// console.log(secondsToMinutes(3661)); // "61 minutes 1 second"
 // console.log(formatDuration(1), "1 second");
 // console.log(formatDuration(62), "1 minute and 2 seconds");
 // console.log(formatDuration(120), "2 minutes");
