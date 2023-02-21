a44fbafa2045ea7f3b7756a710ab8bab9ac4b1a9
Tony Wang <wangh9947@gmail.com>
2023-02-21 20:08:48 +0000

    refactor round1

--- kyu4/humanRead.js
+++ kyu4/humanRead.js
@@ -1,41 +1,44 @@
 const getUnformattedTime = (seconds) => {
-  let minutes = Math.floor(seconds / 60);
-  let remainingSeconds = seconds % 60;
-  if (minutes >= 60) {
-    let hours = Math.floor(minutes / 60);
-    let remainingMinutes = minutes % 60;
-    if (hours >= 24) {
-      let days = Math.floor(hours / 24);
-      let remainingHours = hours % 24;
+  if (seconds >= 60) {
+    let minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
 
-      if (days >= 30) {
-        let months = Math.floor(days / 30);
-        let remainingDays = days % 30;
+    if (minutes >= 60) {
+      let hours = Math.floor(minutes / 60);
+      let remainingMinutes = minutes % 60;
+      if (hours >= 24) {
+        let days = Math.floor(hours / 24);
+        let remainingHours = hours % 24;
+
+        if (days >= 30) {
+          let months = Math.floor(days / 30);
+          let remainingDays = days % 30;
+          return {
+            months: months,
+            days: remainingDays,
+            hours: remainingHours,
+            minutes: remainingMinutes,
+            seconds: remainingSeconds,
+          };
+        }
         return {
-          months: months,
-          days: remainingDays,
+          days: days,
           hours: remainingHours,
           minutes: remainingMinutes,
           seconds: remainingSeconds,
         };
       }
       return {
-        days: days,
-        hours: remainingHours,
+        hours: hours,
         minutes: remainingMinutes,
         seconds: remainingSeconds,
       };
     }
-    return {
-      hours: hours,
-      minutes: remainingMinutes,
-      seconds: remainingSeconds,
-    };
+    return { minutes: minutes, seconds: remainingSeconds };
   }
-
-  return { minutes: minutes, seconds: remainingSeconds };
+  return { seconds: seconds };
 };
-
+console.log(getUnformattedTime(3), { seconds: 3 });
 console.log(getUnformattedTime(62), { minutes: 1, seconds: 2 });
 console.log(getUnformattedTime(3661), { hours: 1, minutes: 1, seconds: 1 });
 console.log(getUnformattedTime(3609), { hours: 1, minutes: 0, sseconds: 9 });
