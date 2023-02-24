2957d168d18fb1b5e90a6500543357b86c0a9ec3
Tony Wang <wangh9947@gmail.com>
2023-02-24 22:14:16 +0000

    multi table passed all tests

--- kyu6/multiplicationTable.js
+++ kyu6/multiplicationTable.js
@@ -4,7 +4,11 @@ multiplicationTable = function (size) {
     let row1 = [];
 
     for (let j = 1; j <= size; j++) {
-      
+      let product = i * j;
+      row1.push(product);
     }
+    multiTable.push(row1);
   }
+  return multiTable;
 };
+console.log(multiplicationTable(3)); //[[1, 2, 3], [2, 4, 6], [3, 6, 9]]
