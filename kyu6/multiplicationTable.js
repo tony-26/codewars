e72527b3b3fb05875c03114da1cbdf3dc4831ace
Tony Wang <wangh9947@gmail.com>
2023-02-24 22:09:40 +0000

    multiplication table try

---
+++ kyu6/multiplicationTable.js
@@ -0,0 +1,10 @@
+multiplicationTable = function (size) {
+  let multiTable = [];
+  for (let i = 1; i <= size; i++) {
+    let row1 = [];
+
+    for (let j = 1; j <= size; j++) {
+      
+    }
+  }
+};
