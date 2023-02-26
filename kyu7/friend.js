69f2d8e502a64154df249d4170051829c44c68c4
Tony Wang <wangh9947@gmail.com>
2023-02-26 20:21:29 +0000

    friend filter way

--- kyu7/friend.js
+++ kyu7/friend.js
@@ -1,11 +1,11 @@
 function friend(friends) {
-  let result = [];
-  for (let i = 0; i < friends.length; i++) {
-    if (friends[i].length === 4) {
-      result.push(friends[i]);
-    }
-  }
-  return result;
+  //   let result = [];
+  //   for (let i = 0; i < friends.length; i++) {
+  //     if (friends[i].length === 4) {
+  //       result.push(friends[i]);
+  //     }
+  //   }
+  return friends.filter((friends) => friends.length === 4);
 }
 console.log(friend(["Ryan", "Kieran", "Mark"]), ["Ryan", "Mark"]);
 console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]), ["Ryan"]);
