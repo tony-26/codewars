ce4cc86150ff5f1a60cca7119818cf8a5fdb5d01
Tony Wang <wangh9947@gmail.com>
2023-02-28 00:33:30 +0000

    dirReduc try

--- kyu5/dirReduc.js
+++ kyu5/dirReduc.js
@@ -1,24 +1,40 @@
 function dirReduc(arr) {
-    let x = 0
-    let y = 0
-    for (let i = 0;i<arr.length;i++){
-        if (arr[i]==="NORTH"){
-            y+=1
-        }else if (arr[i]==="SOUTH"){
-            y-=1
-        }else if(arr[i]==="EAST"){
-            x+=1
-        } else if(arr[i]==="WEST"){
-            x-=1
-        }
+  let x = 0;
+  let y = 0;
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "NORTH") {
+      y += 1;
+    } else if (arr[i] === "SOUTH") {
+      y -= 1;
+    } else if (arr[i] === "EAST") {
+      x += 1;
+    } else if (arr[i] === "WEST") {
+      x -= 1;
     }
+  }
+  if (x === 1) {
+    result.push("EAST");
+  }
+  if (x === -1) {
+    result.push("WEST");
+  }
+  if (y === 1) {
+    result.push("NORTH");
+  }
+  if (y === -1) {
+    result.push("SOUTH");
+  }
+  return result;
 }
 console.log(
-  ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"],
-  ["WEST"]
+  dirReduc(
+    ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
+    ["WEST"]
+  )
 );
-console.log(
-  ["NORTH", "WEST", "SOUTH", "EAST"],
-  ["NORTH", "WEST", "SOUTH", "EAST"]
-);
-console.log(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"], []);
+// console.log(
+//   ["NORTH", "WEST", "SOUTH", "EAST"],
+//   ["NORTH", "WEST", "SOUTH", "EAST"]
+// );
+// console.log(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"], []);
