33f48644f1c2a82938b934484ec28904b7865500
Tony Wang <wangh9947@gmail.com>
2023-03-01 21:12:21 +0000

    arithmetic

---
+++ kyu7/arithmetic.js
@@ -0,0 +1,14 @@
+function arithmetic(a, b, operator) {
+  if (operator === "add") {
+    return a + b;
+  }
+  if (operator === "subtract") {
+    return a - b;
+  }
+  if (operator === "multiply") {
+    return a * b;
+  }
+  if (operator === "divide") {
+    return a / b;
+  }
+}
