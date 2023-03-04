06dca8890a0ff463d2530bb7df5da3af492bd56a
Tony Wang <wangh9947@gmail.com>
2023-03-04 10:51:15 +0000

    validParentheses tests passed

--- kyu5/validParentheses.js
+++ kyu5/validParentheses.js
@@ -1,17 +1,21 @@
 function validParentheses(parens) {
   if (parens.length === 0) {
     return true;
+  } else {
+    if (parens === parens.replace("()", "")) {
+      return false;
+    } else {
+      return validParentheses(parens.replace("()", ""));
+    }
   }
-  if (parens.length%2!==0){
-    return false
-  }
-  for (let i=0;i<parens.length;i++){
-    
-  }
-  return false;
 }
-console.log(validParentheses("("), false);
-console.log(validParentheses(")"), false);
-console.log(validParentheses(""), true);
-console.log(validParentheses("()"), true);
-console.log(validParentheses("())"), false);
+// console.log(validParentheses("("), false);
+// console.log(validParentheses(")"), false);
+// console.log(validParentheses(""), true);
+// console.log(validParentheses("()"), true);
+// console.log(validParentheses("())"), false);
+// console.log(validParentheses("(()(()()))"));
+//step1: looking for a pair of "()", if not fund return false
+//step2: if fund, remove "()" => "((()()))"
+//step3: if string is empty, return true, else:  repeat step1 and 2 =>
+//
