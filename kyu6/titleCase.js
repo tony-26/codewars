0e3bc741d8fdbba0136328650a16023b688f30c9
Tony Wang <wangh9947@gmail.com>
2023-03-10 18:54:36 +0000

    sayhello

---
+++ kyu6/titleCase.js
@@ -0,0 +1,13 @@
+function titleCase(title, minorWords) {
+    if (title.length===0){
+        return ""
+    }
+    
+}
+console.log(titleCase(""), "");
+console.log(titleCase("a clash of KINGS", "a an the of"), "A Clash of Kings");
+console.log(
+  titleCase("THE WIND IN THE WILLOWS", "The In"),
+  "The Wind in the Willows"
+);
+console.log(titleCase("the quick brown fox"), "The Quick Brown Fox");
---
+++ kyu8/sayHelloo.js
@@ -0,0 +1,4 @@
+function sayHello(name, city, state) {
+  let fullName = name.join(" ");
+  return "Hello, " + fullName + "! Welcome to " + city + ", " + state + "!";
+}
