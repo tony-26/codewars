7d0e1ecc96a9227648c2a895681af763c1bd9096
Tony Wang <wangh9947@gmail.com>
2023-03-10 17:20:25 +0000

    string split

--- kyu6/solution3.js
+++ kyu6/solution3.js
@@ -1,4 +1,8 @@
 function solution(str) {
+  if (str.length === 0) {
+    return [];
+  }
+
   let result = [];
   if (str.length % 2 === 0) {
     for (let i = 0; i < str.legnth; i += 2) {
@@ -6,6 +10,13 @@ function solution(str) {
       result.push(str[i], str[i + 1]);
     }
   }
+  if (str.length % 2 !== 0) {
+    for (let i = 0; i < str.length; i += 1) {
+      result.push(str[i], str[i + 1]);
+    }
+    result[result.length - 1] = result[result.length - 1] + "_";
+  }
+
   return result;
 }
 console.log(solution("abcdef"), ["ab", "cd", "ef"]);
