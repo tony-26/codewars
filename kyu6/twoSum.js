3f105f3a5ae45494215cecddacf1b2ae5b9233e0
Tony Wang <wangh9947@gmail.com>
2023-03-14 12:10:43 +0000

    twoSum test working

--- kyu6/twoSum.js
+++ kyu6/twoSum.js
@@ -1,12 +1,11 @@
 function twoSum(numbers, target) {
   let result = [];
-  result.push(number[0]);
-  let difference = target - number[0];
-  let i = 1;
-  if (result.length === 2 && result[0] + result[1] === target) return result;
-  else {
-    if 
-    i += 1;
+  result.push(0);
+  for (let i = 1; i < numbers.length; i++) {
+    if (target - numbers[0] === numbers[i]) {
+      result.push(i);
+    }
   }
+  return result;
 }
 console.log(twoSum([1, 2, 3], 4)); //[0, 2]
