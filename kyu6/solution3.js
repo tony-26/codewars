df61ed5900aed4624b58f02fc6077bcd116cd38b
Tony Wang <wangh9947@gmail.com>
2023-03-14 10:51:40 +0000

    odd number works

--- kyu6/solution3.js
+++ kyu6/solution3.js
@@ -8,20 +8,21 @@ function solution(str) {
   if (str.length % 2 === 0) {
     //console.log("hi");
     for (let i = 0; i < str.length; i += 2) {
-      console.log(i);
+      //console.log(i);
       pairedStr = str[i] + str[i + 1];
       result.push(pairedStr);
     }
   }
-  //   if (str.length % 2 !== 0) {
-  //     for (let i = 0; i < str.length; i += 1) {
-  //       result.push(str[i], str[i + 1]);
-  //     }
-  //     result[result.length - 1] = result[result.length - 1] + "_";
-  //   }
+  if (str.length % 2 !== 0) {
+    str = str + "_";
+    for (let i = 0; i < str.length; i += 2) {
+      pairedStr = str[i] + str[i + 1];
+      result.push(pairedStr);
+    }
+  }
 
   return result;
 }
-console.log(solution("abcdef"), ["ab", "cd", "ef"]);
-// console.log(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
+//console.log(solution("abcdef"), ["ab", "cd", "ef"]);
+console.log(solution("abcdefg"), ["ab", "cd", "ef", "g_"]);
 // console.log(solution(""), []);
