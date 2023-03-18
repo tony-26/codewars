468e70cf5316e89ba5b79c929cc836b22d59da51
Tony Wang <wangh9947@gmail.com>
2023-03-18 09:50:44 +0000

    2 digits working

--- kyu4/getPINs.js
+++ kyu4/getPINs.js
@@ -15,5 +15,23 @@ const getSingleDigit = (str) => {
   };
   return pinPossibility[str].split("");
 };
-console.log(getSingleDigit("8")); //[5, 7, 8, 9, 0]
-console.log(getSingleDigit("9")); //[6, 8, 9]
+// console.log(getSingleDigit("8")); //[5, 7, 8, 9, 0]
+// console.log(getSingleDigit("9")); //[6, 8, 9]
+
+const possiblePasswords = (str) => {
+  let arr = [];
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    arr.push(getSingleDigit(str[i]));
+  }
+  for (let i = 0; i < arr[0].length; i++) {
+    for (let j = 0; j < arr[1].length; j++) {
+      let processingPin = arr[0][i].toString();
+      processingPin = processingPin + arr[1][j].toString();
+      result.push(processingPin);
+      processingPin.split("").pop();
+    }
+  }
+  return result;
+};
+console.log(possiblePasswords("89")); //[56, 58, 59, 76,78, 79, 86, 88, 89 ,96, 98, 99,06, 08, 09]
