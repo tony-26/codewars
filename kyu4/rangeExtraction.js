0e39d26472c692d421a29d21d64ed0b88c92c7da
Tony Wang <wangh9947@gmail.com>
2023-03-21 18:20:35 +0000

    range... working with len4

--- kyu4/rangeExtraction.js
+++ kyu4/rangeExtraction.js
@@ -1,3 +1,19 @@
-const rangeExtraction = (list) => {};
+const rangeExtraction = (list) => {
+  let continueStr = "";
+  let result = [];
+  let firstNum = list[0];
+  for (let i = 0; i < list.length; i++) {
+    if (list[i + 1] - list[i] > 1) {
+      continueStr = firstNum + "-" + list[i];
+      result.push(continueStr);
+    }
+    if (list[i] - list[i - 1] !== 1 && list[i + 1] - list[i] !== 1) {
+      result.push(list[i]);
+    }
+  }
+  return result.join(",");
+};
+//console.log(rangeExtraction([1, 2, 3])); //"1-3"
+console.log(rangeExtraction([1, 2, 3, 5])); //"1-3,5"
 console.log(rangeExtraction([2, 3, 4, 5, 10, 11, 12, 13])); // "2-5,10-13"
-console.log(rangeExtraction([2, 3, 4, 5, 6, 10, 11, 15, 16, 17])); // "2-6,10,11,15-17"
+// console.log(rangeExtraction([2, 3, 4, 5, 6, 10, 11, 15, 16, 17])); // "2-6,10,11,15-17"
