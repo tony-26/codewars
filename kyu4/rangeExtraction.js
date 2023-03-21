cdbbe68685128d80874deacce69441f4b05bdd16
Tony Wang <wangh9947@gmail.com>
2023-03-21 18:52:38 +0000

    partly working

--- kyu4/rangeExtraction.js
+++ kyu4/rangeExtraction.js
@@ -1,17 +1,38 @@
-const rangeExtraction = (list) => {
-  let continueStr = "";
-  let result = [];
-  let firstNum = list[0];
-  for (let i = 0; i < list.length; i++) {
-    if (list[i + 1] - list[i] > 1) {
-      continueStr = firstNum + "-" + list[i];
-      result.push(continueStr);
-    }
-    if (list[i] - list[i - 1] !== 1 && list[i + 1] - list[i] !== 1) {
-      result.push(list[i]);
+const rangeExtraction = (list, result = []) => {
+  let output = "";
+  let begin = list[0];
+  let end = list[0];
+  for (let i = 1; i < list.length; i++) {
+    if (list[i] === end + 1) {
+      end = list[i];
+    } else {
+      if (begin === end) {
+        output = output + begin + ",";
+      }
+      output += begin + "-" + end;
+      begin = list[i];
+      end = list[i];
     }
   }
-  return result.join(",");
+  return output;
+
+  //   if (list.length === 0) {
+  //     return result.join(",");
+  //   }
+  //   let continueStr = "";
+  //   let firstNum = list[0];
+  //   const length = list.length;
+  //   for (let i = 0; i < length; i++) {
+  //     if (list[i + 1] - list[i] > 1) {
+  //       continueStr = firstNum + "-" + list[i];
+  //       result.push(continueStr);
+  //       list.splice(0, i);
+  //     }
+  //     if (list[i] - list[i - 1] !== 1 && list[i + 1] - list[i] !== 1) {
+  //       result.push(list[i]);
+  //     }
+  //   }
+  //   return rangeExtraction(list, result);
 };
 //console.log(rangeExtraction([1, 2, 3])); //"1-3"
 console.log(rangeExtraction([1, 2, 3, 5])); //"1-3,5"
