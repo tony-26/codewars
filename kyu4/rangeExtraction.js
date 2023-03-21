903463493c6f7c67de1c2947cb8dd4b6597d62b0
Tony Wang <wangh9947@gmail.com>
2023-03-21 19:15:36 +0000

    shiftStartingRange method

--- kyu4/rangeExtraction.js
+++ kyu4/rangeExtraction.js
@@ -1,40 +1,38 @@
-const rangeExtraction = (list, result = []) => {
-  let output = "";
-  let begin = list[0];
-  let end = list[0];
-  for (let i = 1; i < list.length; i++) {
-    if (list[i] === end + 1) {
-      end = list[i];
+const shiftStartingRange = (list) => {
+  const result = [list.shift()];
+  while (true) {
+    if (list[0] - result[result.length - 1] === 1) {
+      result.push(list.shift());
     } else {
-      if (begin === end) {
-        output = output + begin + ",";
-      }
-      output += begin + "-" + end;
-      begin = list[i];
-      end = list[i];
+      return result;
     }
   }
-  return output;
+};
 
-  //   if (list.length === 0) {
-  //     return result.join(",");
-  //   }
-  //   let continueStr = "";
-  //   let firstNum = list[0];
-  //   const length = list.length;
-  //   for (let i = 0; i < length; i++) {
-  //     if (list[i + 1] - list[i] > 1) {
-  //       continueStr = firstNum + "-" + list[i];
-  //       result.push(continueStr);
-  //       list.splice(0, i);
+const arr = [2, 3, 4, 5, 6, 7, 8, 9];
+const result = shiftStartingRange(arr);
+console.log(result, arr); //[2, 3, 4, 5], [7, 8, 9]
+
+const rangeExtraction = (list) => {
+  //   let output = "";
+  //   let begin = list[0];
+  //   let end = list[0];
+  //   for (let i = 1; i < list.length; i++) {
+  //     if (list[i] === end + 1) {
+  //       end = list[i];
+  //     } else {
+  //       if (begin === end) {
+  //         output = output + begin + ",";
+  //       } else {
+  //         output += begin + "-" + end + ",";
+  //       }
+  //       begin = list[i + 1];
+  //       end = list[i + 1];
   //     }
-  //     if (list[i] - list[i - 1] !== 1 && list[i + 1] - list[i] !== 1) {
-  //       result.push(list[i]);
-  //     }
   //   }
-  //   return rangeExtraction(list, result);
+  //   return output;
 };
 //console.log(rangeExtraction([1, 2, 3])); //"1-3"
-console.log(rangeExtraction([1, 2, 3, 5])); //"1-3,5"
-console.log(rangeExtraction([2, 3, 4, 5, 10, 11, 12, 13])); // "2-5,10-13"
+//console.log(rangeExtraction([1, 2, 3, 5])); //"1-3,5"
+console.log(rangeExtraction([2, 3, 5, 10, 11, 12, 13, 15, 16, 19])); // "2-5,10-13"
 // console.log(rangeExtraction([2, 3, 4, 5, 6, 10, 11, 15, 16, 17])); // "2-6,10,11,15-17"
