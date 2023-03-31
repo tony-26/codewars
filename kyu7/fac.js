ff7b7c31334137402ccc8efb3e14692606e325f4
Tony Wang <wangh9947@gmail.com>
2023-03-31 14:37:04 +0100

    fac done

---
+++ kyu7/fac.js
@@ -0,0 +1,12 @@
+const factorial = (n) => {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+};
+console.log(factorial(0), 1);
+console.log(factorial(1), 1);
+console.log(factorial(4), 24);
+console.log(factorial(7), 5040);
+console.log(factorial(17), 355687428096000);
