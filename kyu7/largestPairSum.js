6deb50b60a08a58bf624487abd47a2ca61383b8b
Tony Wang <wangh9947@gmail.com>
2023-04-03 22:54:44 +0100

    largestpiar sum

--- kyu7/largestPairSum.js
+++ kyu7/largestPairSum.js
@@ -1,6 +1,6 @@
 function largestPairSum(numbers) {
-  let max1 = numbers[0];
-  let max2 = numbers[1];
+  let max1 = Math.max(numbers[0], numbers[1]);
+  let max2 = Math.min(numbers[0], numbers[1]);
   for (let i = 2; i < numbers.length; i++) {
     if (numbers[i] > max1) {
       max2 = max1;
@@ -11,6 +11,7 @@ function largestPairSum(numbers) {
   }
   return max1 + max2;
 }
+
 console.log(largestPairSum([10, 14, 2, 23, 19], 42));
 console.log(largestPairSum([-100, -29, -24, -19, 19], 0));
 console.log(largestPairSum([1, 2, 3, 4, 6, -1, 2], 10));
