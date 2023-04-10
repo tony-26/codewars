2deb2ed9ae527fb37aaa3ae4ed9afea03759149d
Tony Wang <wangh9947@gmail.com>
2023-04-10 22:26:02 +0100

    rgb fixed remaining value

--- kyu5/rgb.js
+++ kyu5/rgb.js
@@ -3,7 +3,8 @@ const rgb = (r, g, b) => {
   const hex = { 10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F" };
 
   let firstDigitR = Math.floor(r / 16);
-  let remainingR = r - firstDigitR;
+  let remainingR = r - firstDigitR * 16;
+  console.log(remainingR);
   if (firstDigitR > 10) {
     firstDigitR = hex[firstDigitR];
   }
