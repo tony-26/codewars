9a6676d8b18897bb3499a80843eb99c1950489d7
Tony Wang <wangh9947@gmail.com>
2023-04-10 22:37:59 +0100

    rgb passed tests

--- kyu5/rgb.js
+++ kyu5/rgb.js
@@ -1,23 +1,28 @@
 const rgb = (r, g, b) => {
-  let hexR = "";
+  if (r <= 0 && g <= 0 && b <= 0) {
+    return "000000";
+  }
+  let arr = [r, g, b];
+  let hexV = "";
   const hex = { 10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F" };
+  for (let i = 0; i < arr.length; i++) {
+    let firstDigitR = Math.floor(arr[i] / 16);
+    let remainingR = arr[i] - firstDigitR * 16;
+    //console.log(remainingR);
+    if (firstDigitR >= 10) {
+      firstDigitR = hex[firstDigitR];
+    }
+    if (remainingR > 10) {
+      remainingR = hex[remainingR];
+    }
+    hexV = hexV + firstDigitR.toString() + remainingR.toString();
+  }
 
-  let firstDigitR = Math.floor(r / 16);
-  let remainingR = r - firstDigitR * 16;
-  console.log(remainingR);
-  if (firstDigitR > 10) {
-    firstDigitR = hex[firstDigitR];
-  }
-  if (remainingR > 10) {
-    remainingR = hex[remainingR];
-  }
-  console.log([firstDigitR, remainingR]);
-  // hexR = firstDigitR.toString() + remainingR.toString();
-  return hexR;
+  return hexV;
 };
 // console.log(rgb(0, 0, 0), "000000");
 // console.log(rgb(0, 0, -20), "000000");
 //console.log(rgb(300, 255, 255), "FFFFFF");
-console.log(rgb(173, 255, 47), "ADFF2F");
+console.log(rgb(173, 255, 47)); //"ADFF2F"
 
 // console.log(Math.floor(100 / 3));
