4aca132d57963000e59d9955d228c2fbb1ee5959
Tony Wang <wangh9947@gmail.com>
2023-04-10 19:47:03 +0100

    rgb processing

--- kyu5/rgb.js
+++ kyu5/rgb.js
@@ -1,17 +1,22 @@
 const rgb = (r, g, b) => {
   let hexR = "";
-  const hex = { 11: "A", 12: "B", 13: "C", 14: "D", 15: "E", 16: "F" };
-  while (r > 0) {
-    let firstDigitR = Math.floor(r / 16);
-    let remainingR = r - firstDigitR;
-    if (firstDigitR > 10) {
-      firstDigitR = hex[firstDigitR];
-    }
-    if (remainingR > 10) {
-      remainingR = hex[remainingR];
-    }
-    hexR = firstDigitR.toString() + remainingR.toString();
+  const hex = { 10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F" };
+
+  let firstDigitR = Math.floor(r / 16);
+  let remainingR = r - firstDigitR;
+  if (firstDigitR > 10) {
+    firstDigitR = hex[firstDigitR];
   }
-  
+  if (remainingR > 10) {
+    remainingR = hex[remainingR];
+  }
+  console.log([firstDigitR, remainingR]);
+  // hexR = firstDigitR.toString() + remainingR.toString();
+  return hexR;
 };
-console.log(Math.floor(100 / 3));
+// console.log(rgb(0, 0, 0), "000000");
+// console.log(rgb(0, 0, -20), "000000");
+//console.log(rgb(300, 255, 255), "FFFFFF");
+console.log(rgb(173, 255, 47), "ADFF2F");
+
+// console.log(Math.floor(100 / 3));
