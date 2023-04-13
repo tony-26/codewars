f294eed7a7e1efa0841d2d672d85b07e2bb20148
Tony Wang <wangh9947@gmail.com>
2023-04-13 16:22:52 +0100

    sleight done

--- kyu6/encrypt.js
+++ kyu6/encrypt.js
@@ -1,7 +1,7 @@
 const encrypt = (text, n) => {
   let arr1 = [];
   let arr2 = [];
-
+  let newArr = [];
   for (let i = 0; i < text.length; i++) {
     if (i % 2 === 0) {
       arr1.push(text[i]);
@@ -9,6 +9,7 @@ const encrypt = (text, n) => {
       arr2.push(text[i]);
     }
   }
-  return arr2.concat(arr1).join("");
+  newArr = arr2.concat(arr1).join("");
+  return newArr;
 };
 console.log(encrypt("012345", 1)); //"135024"
