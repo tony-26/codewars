15568845ee1c12896c1a8eeb0a10a467e55b3bbd
Tony Wang <wangh9947@gmail.com>
2023-04-13 17:03:42 +0100

    encrypt trying recurssion

--- kyu6/encrypt.js
+++ kyu6/encrypt.js
@@ -1,7 +1,9 @@
 const encrypt = (text, n) => {
+  let result = "";
+  if (n === 0) return result;
   let arr1 = [];
   let arr2 = [];
-  let newArr = [];
+
   for (let i = 0; i < text.length; i++) {
     if (i % 2 === 0) {
       arr1.push(text[i]);
@@ -9,7 +11,11 @@ const encrypt = (text, n) => {
       arr2.push(text[i]);
     }
   }
-  newArr = arr2.concat(arr1).join("");
-  return newArr;
+  n = n - 1;
+  result = arr2.concat(arr1).join("");
+  return encrypt(result, n);
 };
-console.log(encrypt("012345", 1)); //"135024"
+
+console.log(encrypt("012345", 3)); //"012345"
+
+//console.log(encrypt("012345", 1)); //"135024"
