a9daeb1f3d2cf8d63ef04796b1cc6cab19a0cdef
tony <wangh9947@gmail.com>
2023-10-26 23:03:27 +0100

    maths operation

---
+++ kyu8/basic_op.py
@@ -0,0 +1,11 @@
+def basic_op(operator, value1, value2):
+
+    if operator == '+':
+        return value1 + value2
+    elif operator == '-':
+        return value1 - value2
+    elif operator == '*':
+        return value1 * value2
+    elif operator == '/':
+        if value2 != 0:
+            return value1 / value2
