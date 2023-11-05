43aca6d4d95622f2c5bced096d4814825d4c055e
tony <wangh9947@gmail.com>
2023-11-05 21:14:26 +0000

    factored version

--- kyu8/uefa_euro_2016.py
+++ kyu8/uefa_euro_2016.py
@@ -11,3 +11,9 @@ def uefa_euro_2016(teams, scores):
             teams[1]+", teams played draw."
 
     return str
+
+
+def uefa_euro_2016(teams, scores):
+    match_result = "won!" if scores[0] != scores[1] else "teams played draw."
+    winning_team = teams[0] if scores[0] > scores[1] else teams[1]
+    return "At match {} - {}, {} {}".format(teams[0], teams[1], "teams played draw" if scores[0] == scores[1] else winning_team, match_result)
