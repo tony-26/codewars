eecd2e4af33ebecdd7b3a54f642ba8036ab73add
tony <wangh9947@gmail.com>
2023-11-07 22:35:10 +0000

    calculator right version

--- kyu8/calculator.py
+++ kyu8/calculator.py
@@ -1,15 +1,20 @@
 def calculator(x, y, op):
     arr = ["+", "-", "*", "/"]
+    print(x, y, op)
     if op not in arr:
+        return "unknown value"
+    if not isinstance(y, int):
+        return "unknown value"
+    if not isinstance(x, int):
         return "unknown value"
 
     if op == '+':
-        return x + y
+        return int(x) + int(y)
     elif op == '-':
-        return x - y
+        return int(x) - int(y)
     elif op == '*':
-        return x * y
+        return int(x) * int(y)
     elif op == '/':
         if y == 0:
             return "unknown value"
-        return x / y
+        return int(x) / int(y)
