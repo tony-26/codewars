6ce55be7b71e50eec0a6cbfb87144590487d15e5
tony <wangh9947@gmail.com>
2023-11-15 00:20:03 +0000

    encrypt and decrypt

--- kyu6/encrypt.js
+++ kyu6/encrypt.js
@@ -1,18 +1,32 @@
 const encrypt = (text, n) => {
-  if (n === 0) return text;
-
-  const arr1 = [];
-  const arr2 = [];
-  for (let i = 0; i < text.length; i++) {
-    if (i % 2 === 0) {
-      arr1.push(text[i]);
-    } else {
-      arr2.push(text[i]);
+  for (let i = 0; i < n; i++) {
+    let odd = "";
+    let even = "";
+    for (let j = 0; j < text.length; j++) {
+      if (j % 2 === 0) {
+        even += text[j];
+      } else {
+        odd += text[j];
+      }
     }
+    text = odd + even;
   }
-  return encrypt(arr2.concat(arr1).join(""), n - 1);
+  return text;
 };
 
+const decrypt = (encryptedText, n) => {
+  for (let i = 0; i < n; i++) {
+    const halfLength = Math.floor(encryptedText.length / 2);
+    const firstHalf = encryptedText.slice(0, halfLength);
+    const secondHalf = encryptedText.slice(halfLength);
+    let decryptedText = "";
+    for (let j = 0; j < halfLength; j++) {
+      decryptedText += (secondHalf[j] || "") + (firstHalf[j] || "");
+    }
+    encryptedText = decryptedText;
+  }
+  return encryptedText;
+};
 console.log(encrypt("012345", 3)); //"012345"
 
-//console.log(encrypt("012345", 1)); //"135024"
+console.log(encrypt("012345", 1)); //"135024"
